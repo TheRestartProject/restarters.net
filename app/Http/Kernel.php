c44608689f57a04a83b5ce2c6eb68db1fae36462
Will Prentice <willprentice@Will-MBP>
2018-05-15 15:18:01 +0100

    Force HTTPS

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,6 +19,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        \App\Http\Middleware\HttpsProtocol::class,
     ];
 
     /**
@@ -59,6 +60,5 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'forceHttps' => \App\Http\Middleware\HttpsProtocol::class,
     ];
 }
