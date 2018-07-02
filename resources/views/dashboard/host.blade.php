73ee8be4c393a6a02105e0cb2473510bfb988709
Neil Mather <ngmather@gmail.com>
2018-07-02 14:21:01 +0100

    Tweaks to dashboard

--- resources/views/dashboard/host.blade.php
+++ resources/views/dashboard/host.blade.php
@@ -3,13 +3,13 @@
 @endif
 <div class="col-lg-6 col-dashboard">@include('partials.past')</div>
 @if ($user->number_of_logins == 2)
-  <div class="col-lg-6 col-dashboard">@include('partials.how-to')</div>
+  <div class="col-lg-4 col-dashboard">@include('partials.how-to')</div>
 @endif
 @if (!empty($all_groups))
-  <div class="col-lg-6 col-dashboard">@include('partials.in-your-area')</div>
+  <div class="col-lg-4 col-dashboard">@include('partials.in-your-area')</div>
 @endif
 @if (!empty($outdated_groups))
-  <div class="col-lg-6 col-dashboard">@include('partials.up-to-date')</div>
+  <div class="col-lg-4 col-dashboard">@include('partials.up-to-date')</div>
 @endif
-<div class="col-lg-6 col-dashboard">@include('partials.wiki')</div>
-<div class="col-lg-6 col-dashboard">@include('partials.discussion')</div>
+<div class="col-lg-4 col-dashboard">@include('partials.wiki')</div>
+<div class="col-lg-4 col-dashboard">@include('partials.discussion')</div>
