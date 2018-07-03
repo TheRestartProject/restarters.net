49365edbc8e1a67b8d0c6fc04115f5dbd91336c4
Dean Appleton-Claydon <dean@omniowl.com>
2018-07-03 14:05:18 +0100

    All groups - pagination

--- resources/views/group/index.blade.php
+++ resources/views/group/index.blade.php
@@ -65,11 +65,7 @@
 
         <div class="d-flex justify-content-center">
           <nav aria-label="Page navigation example">
-            <ul class="pagination">
-              <li class="page-item active"><a class="page-link" href="#">1</a></li>
-              <li class="page-item"><a class="page-link" href="#">2</a></li>
-              <li class="page-item"><a class="page-link" href="#">3</a></li>
-            </ul>
+            {!! $groups->links() !!}
           </nav>
         </div>
 
