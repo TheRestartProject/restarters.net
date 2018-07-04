b258f173a7e6d876cc31e116e60aa8682e81b0ff
Neil Mather <ngmather@gmail.com>
2018-07-04 23:47:35 +0100

    Dashboard tweaks

---
+++ resources/lang/en/dashboard.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Dashboard
+    |--------------------------------------------------------------------------
+    |
+    |
+    */
+        'getting_started_header' => 'Getting Started',
+        'getting_started_text' => 'Welcome to the Restarters community space.  Here are a few actions to get you started:',
+        'add_avatar' => 'Add an avatar',
+        'add_skills' => 'Add your skills',
+        'join_group' => 'Join a group',
+        'rsvp_event' => 'RSVP to an event',
+        'devices_logged' => 'devices logged',
+    ];
