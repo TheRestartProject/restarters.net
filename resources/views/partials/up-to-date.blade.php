3877ae786ed2c373048f9ccdd551e41cb9e7e889
Neil Mather <ngmather@gmail.com>
2018-07-06 15:14:30 +0100

    Fix bug with up-to-date using inactive rather than outdated

--- resources/views/partials/up-to-date.blade.php
+++ resources/views/partials/up-to-date.blade.php
@@ -3,9 +3,9 @@
     <div class="dashboard__block__content">
       <h4>Keep your group information up to date!</h4>
       <p>A fresh profile helps recruit more volunteers. Make sure to link to your website or social media channels.</p>
-        @foreach($inactive_groups as $inactive_group)
+        @foreach($outdated_groups as $outdated_group)
           <div class="dashboard__links d-flex flex-row justify-content-end">
-              <a href="/group/edit/{{ $inactive_group->idgroups }}">Update {{ $inactive_group->name }}</a>
+              <a href="/group/edit/{{ $outdated_group->idgroups }}">Update {{ $outdated_group->name }}</a>
           </div>
         @endforeach
     </div>
