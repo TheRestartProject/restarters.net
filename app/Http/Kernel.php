dfb80583d370a3af1c326a1041b5ac71b2ad50e2
Neil Mather <ngmather@gmail.com>
2018-07-13 15:48:40 +0100

    Disable CORS middleware for now
    Wasn't working on production.  Setting Access-Control-Allow-Origin via Apache
    for now.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,7 +41,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
-            \Barryvdh\Cors\HandleCors::class,
+            //\Barryvdh\Cors\HandleCors::class,
         ],
     ];
 
