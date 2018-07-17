49e64b181876c1f00dd64ebeba4665075b5c5ea1
Dean Appleton-Claydon <dean@omniowl.com>
2018-07-17 16:33:11 +0100

    Image width

--- resources/views/partials/tables/head-groups.blade.php
+++ resources/views/partials/tables/head-groups.blade.php
@@ -1,9 +1,9 @@
 <thead>
   <tr>
-    <th></th>
-    <th scope="col">@lang('groups.groups_name')</th>
-    <th scope="col">@lang('groups.groups_location')</th>
-    <th scope="col" class="text-center">@lang('groups.groups_hosts')</th>
-    <th scope="col" class="text-center">@lang('groups.groups_restarters')</th>
+    <th width="42"></th>
+    <th width="200" scope="col">@lang('groups.groups_name')</th>
+    <th width="200" scope="col">@lang('groups.groups_location')</th>
+    <th width="75" scope="col" class="text-center">@lang('groups.groups_hosts')</th>
+    <th width="75" scope="col" class="text-center">@lang('groups.groups_restarters')</th>
   </tr>
 </thead>
