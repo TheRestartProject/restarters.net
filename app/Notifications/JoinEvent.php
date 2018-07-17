92551388c81280ea6ae46c56bc783a8b7da056b1
Will Prentice <willprentice@Will-MBP>
2018-07-17 14:48:38 +0100

    Event email updates

--- app/Notifications/JoinEvent.php
+++ app/Notifications/JoinEvent.php
@@ -50,6 +50,23 @@ class JoinEvent extends Notification
                           ->subject('Event Invitation')
                           ->greeting('Hello!')
                           ->line('You have received this email because you have been invited by ' . $this->arr['name'] . ' to join the ' . $this->arr['group'] . ' at an upcoming event:')
+                          ->line('')
+                          ->line('
+                            <table style="margin-left:120px; color:black">
+                                    <tr>
+                                      <td>Group:</td>
+                                      <td>'.$this->arr['group'].'</td>
+                                    </tr>')
+                          ->line('<tr>
+                                    <td>Date/time:</td>
+                                    <td>'.$this->arr['event']->event_date.' '.$this->arr['event']->getEventStartEnd().'</td>
+                                  </tr>')
+                          ->line('<tr>
+                                    <td>Location:</td>
+                                    <td>'.$this->arr['event']->location.'</td>
+                                  </tr>
+                            </table>')
+                          ->line('<br>')
                           ->line('You can turn up on the day, or if you would prefer you can also create an account with us and RSVP online.')
                           ->line('')
                           ->line($this->arr['name'] . ' attached this message with the invite:')
@@ -63,6 +80,23 @@ class JoinEvent extends Notification
                           ->subject('Event Invitation')
                           ->greeting('Hello!')
                           ->line('You have received this email because you have been invited by ' . $this->arr['name'] . ' to join the ' . $this->arr['group'] . ' at an upcoming event:')
+                          ->line('')
+                          ->line('
+                            <table style="margin-left:120px; color:black">
+                                    <tr>
+                                      <td>Group:</td>
+                                      <td>'.$this->arr['group'].'</td>
+                                    </tr>')
+                          ->line('<tr>
+                                    <td>Date/time:</td>
+                                    <td>'.$this->arr['event']->event_date.' '.$this->arr['event']->getEventStartEnd().'</td>
+                                  </tr>')
+                          ->line('<tr>
+                                    <td>Location:</td>
+                                    <td>'.$this->arr['event']->location.'</td>
+                                  </tr>
+                            </table>')
+                          ->line('<br>')
                           ->line('You can turn up on the day, or if you would prefer you can also create an account with us and RSVP online.')
                           ->action('RSVP now', $this->arr['url'])
                           ->line('If you think this invitation was not intended for you, please discard this email.');
