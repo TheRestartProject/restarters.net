0f038db578074050d5643f0ea098ee8da7a0a98b
Neil Mather <ngmather@gmail.com>
2018-07-31 12:45:11 +0100

    No background repeat

--- resources/views/dashboard/restarter.blade.php
+++ resources/views/dashboard/restarter.blade.php
@@ -5,11 +5,11 @@
     <div class="col-lg-3 col-dashboard">@include('dashboard.blocks.wiki')</div>
     <div class="col-lg-3 col-dashboard">@include('dashboard.blocks.community-news')</div>
 @else
-    <div class="col-lg-4 col-xl-3 col-dashboard">@include('partials.discussion')</div>
-    <div class="col-lg-8 col-xl-6 col-dashboard">@include('partials.upcoming2')</div>
-    <div class="col-lg-4 col-xl-3 col-dashboard">@include('dashboard.blocks.welcome-materials-restarter')</div>
-    <div class="col-lg-8 col-xl-6 col-dashboard">@include('partials.past')</div>
-    <div class="col-lg-6 col-xl-3 col-dashboard">@include('dashboard.blocks.wiki')</div>
-    <div class="col-lg-6 col-xl-3 col-dashboard">@include('dashboard.blocks.community-news')</div>
+    <div class="col-sm-6 col-md-5 col-lg-4 col-xl-3 col-dashboard">@include('partials.discussion')</div>
+    <div class="col-sm-6 col-md-7 col-lg-8 col-xl-6 col-dashboard">@include('partials.upcoming2')</div>
+    <div class="col-sm-6 col-md-5 col-lg-4 col-xl-3 col-dashboard">@include('dashboard.blocks.welcome-materials-restarter')</div>
+    <div class="col-sm-6 col-md-7 col-lg-8 col-xl-6 col-dashboard">@include('partials.past')</div>
+    <div class="col-sm-6 col-md-5 col-lg-6 col-xl-3 col-dashboard">@include('dashboard.blocks.wiki')</div>
+    <div class="col-sm-6 col-md-5 col-lg-6 col-xl-3 col-dashboard">@include('dashboard.blocks.community-news')</div>
 @endif
 
