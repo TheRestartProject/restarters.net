90409f28d2ba521ec8fb3cfd80cd302f89313aff
Neil Mather <ngmather@gmail.com>
2018-07-31 12:10:05 +0100

    Use pngs over svgs for now
    svgs were not working in chrome.

--- resources/views/dashboard/blocks/impact.blade.php
+++ resources/views/dashboard/blocks/impact.blade.php
@@ -9,14 +9,14 @@
             </div>
             <div class="col-sm-3 col-xl-2 statbox statborder d-flex flex-column justify-content-start align-items-center">
                 <div style="height:52px; margin:10px" class="d-flex align-items-center">
-                    <img style="width:44px" src="/images/dashboard/bin.svg" />
+                    <img style="width:44px" src="/images/dashboard/bin.png" />
                 </div>
                 <div class="statfigure text-center">{{ number_format($impact_stats[0]->total_weights,0,",",",") }} kg</div>
                 <h3 >@lang('partials.waste_prevented')</h3>
             </div>
             <div class="col-sm-3 col-xl-2 statbox statborder d-flex flex-column align-items-center justify-content-start">
                     <div style="height:52px; margin:10px" class="d-flex align-items-center">
-                    <img style="width:58px" src="/images/dashboard/co2.svg" />
+                    <img style="width:58px" src="/images/dashboard/co2.png" />
                 </div>
                 <div class="statfigure text-center">{{ number_format($impact_stats[0]->total_footprints,0,",",",") }} kg</div>
                 <h3>@lang('partials.co2')</h3>
