84e1a929c9930f8de0505a42a32fed2987edf81c
Neil Mather <ngmather@gmail.com>
2018-07-31 13:05:46 +0100

    Fix typo in contact email address on 500 page

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -19,7 +19,7 @@
                     Please let us know that you encountered this issue and we will look into it ASAP.
                 </h2>
                 <p>
-                    You can report the issue by sending an email to <a href="mailto:community@therestartproject.org">commmunity.therestartproject.org</a>, or by posting in the <a href="https://talk.restarters.net/c/restarters-dev">restarters.net development forum</a>.
+                    You can report the issue by sending an email to <a href="mailto:community@therestartproject.org">community@therestartproject.org</a>, or by posting in the <a href="https://talk.restarters.net/c/restarters-dev">restarters.net development forum</a>.
                 </p>
                 <p>
                     Please include the following details in your bug report:
