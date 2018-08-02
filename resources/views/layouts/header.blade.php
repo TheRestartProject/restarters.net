2879436caf3855f87d12e0f70a0e8bc7192f08f7
Neil Mather <ngmather@gmail.com>
2018-08-02 12:45:17 +0100

    Add GTM snippets

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -38,6 +38,11 @@
             gtag('js', new Date());
 
             gtag('config', '{{ env('GOOGLE_ANALYTICS_TRACKING_ID') }}');
+
+            <!-- Google Tag Manager -->
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', {{ env('GOOGLE_TAG_MANAGER_ID') }});
+            <!-- End Google Tag Manager -->
         }
     </script>
   </head>
@@ -47,6 +52,9 @@
   @else
     <body>
   @endif
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ env('GOOGLE_TAG_MANAGER_ID') }}" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 
    <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
--- resources/views/layouts/header_plain.blade.php
+++ resources/views/layouts/header_plain.blade.php
@@ -39,6 +39,11 @@
              gtag('js', new Date());
 
              gtag('config', '{{ env('GOOGLE_ANALYTICS_TRACKING_ID') }}');
+
+             <!-- Google Tag Manager -->
+             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+             })(window,document,'script','dataLayer', {{ env('GOOGLE_TAG_MANAGER_ID') }});
+             <!-- End Google Tag Manager -->
          }
         </script>
 
@@ -49,3 +54,6 @@
     @else
       <body>
     @endif
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ env('GOOGLE_TAG_MANAGER_ID') }}" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
