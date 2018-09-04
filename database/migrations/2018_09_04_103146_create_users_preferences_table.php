1a614ddc1ccf0e5bbbf5ca86a06c5f155974eb27
Chris Kelker <chris@wecreatedigital.co.uk>
2018-09-04 10:58:12 +0100

    Ammended Foreign key user mistake

--- database/migrations/2018_09_04_103146_create_users_preferences_table.php
+++ database/migrations/2018_09_04_103146_create_users_preferences_table.php
@@ -16,7 +16,7 @@ class CreateUsersPreferencesTable extends Migration
         Schema::create('users_preferences', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('preferences');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
             $table->softDeletes();
         });
