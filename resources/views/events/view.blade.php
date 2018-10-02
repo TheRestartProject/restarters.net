ca73493e47912b37d5f6bee259d2ec74338dde1a
Chris Kelker <chris@wecreatedigital.co.uk>
2018-10-02 14:01:28 +0100

    Ammended Events Audit Updates Error

--- resources/views/events/view.blade.php
+++ resources/views/events/view.blade.php
@@ -74,11 +74,12 @@
             <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
               <a href="{{ url('/') }}/party/edit/{{ $formdata->id, count($attended), count($invited) }}" class="dropdown-item">Edit event</a>
               @if( !$event->isInProgress() || !$event->hasFinished() )
-              <a href="{{ url('/') }}/party/delete/{{ $formdata->id }}" id="deleteEvent" class="dropdown-item">Delete event</a>
+              <form action="{{ url('/') }}/party/delete/{{ $formdata->id }}" method="post">
+                <input type="hidden" name="attended" id="countAttended" value="{{count($attended)}}">
+                <input type="hidden" name="invited" id="countInvited" value="{{count($invited)}}">
+                <input type="hidden" name="volunteers" id="countVolunteers" value="{{ $event->volunteers }}">
+                <button  id="deleteEvent" class="dropdown-item">Delete event</button>
               @endif
-              <input type="hidden" name="attended" id="countAttended" value="{{count($attended)}}">
-              <input type="hidden" name="invited" id="countInvited" value="{{count($invited)}}">
-              <input type="hidden" name="volunteers" id="countVolunteers" value="{{ $event->volunteers }}">
               @if( $event->hasFinished() )
               <button data-toggle="modal" data-target="#event-share-stats" class="btn dropdown-item">Share event stats</button>
               <a href="/party/contribution/{{ $formdata->id }}" class="btn dropdown-item">Request contributions</a>
