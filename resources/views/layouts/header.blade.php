5d3755d03f60fca9d433f12f4a18c1045f2b5eb2
Calum Ryan <calum@wecreatedigital.co.uk>
2018-10-02 13:17:03 +0100

    Notifications setup WIP

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -191,6 +191,12 @@
                               <div class="card-body">
                                   <h5 class="card-title">Your group <a href="/group/view/{{{ $event->idgroups }}}">{{{ $event->name }}}</a> is missing devices from the event <a href="/party/view/{{{ $event->idevents }}}">{{{ $event->getEventName() }}}</a></h5>
                                   <time>{{{ $event->getEventDate('D, jS M Y') }}}</time>
+                                  <div class="d-flex flex-row justify-content-end pt-3">
+                                    <button class="btn-marked"><svg width="13px" height="9" viewBox="0 0 54 37" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><title>Green tick icon</title><path d="M4.615 14.064a.969.969 0 0 0-1.334 0l-3 2.979a.868.868 0 0 0 0 1.279l18.334 18c.333.35.916.35 1.291 0l3.042-2.983a.869.869 0 0 0 0-1.28L4.615 14.064z" fill="#0394a6"/><path d="M53.365 4.584a.913.913 0 0 0 .041-1.287L50.365.272c-.334-.358-.959-.363-1.292-.013L15.99 32.109a.873.873 0 0 0 0 1.284l3 3.029a.97.97 0 0 0 1.333.012l33.042-31.85z" fill="#0394a6"/></svg> Marked as read</button>
+                                  </div>
+                                  <div class="d-flex flex-row justify-content-end pt-3 status-read">
+                                    <button class="btn-marked"><svg width="13px" height="9" viewBox="0 0 54 37" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><title>Green tick icon</title><path d="M4.615 14.064a.969.969 0 0 0-1.334 0l-3 2.979a.868.868 0 0 0 0 1.279l18.334 18c.333.35.916.35 1.291 0l3.042-2.983a.869.869 0 0 0 0-1.28L4.615 14.064z" fill="#0394a6"/><path d="M53.365 4.584a.913.913 0 0 0 .041-1.287L50.365.272c-.334-.358-.959-.363-1.292-.013L15.99 32.109a.873.873 0 0 0 0 1.284l3 3.029a.97.97 0 0 0 1.333.012l33.042-31.85z" fill="#0394a6"/></svg> Marked as read</button>
+                                  </div>
                               </div>
                           </div>
                           @endforeach
