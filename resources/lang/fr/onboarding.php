3b5223e9acf513a9998e1b7ac6b4fe325b7c499d
NikhitaPaturu <nikhitapaturu9@gmail.com>
2018-11-27 18:12:18 +0000

    made changes to fr folder

--- resources/lang/fr/onboarding.php
+++ resources/lang/fr/onboarding.php
@@ -9,14 +9,15 @@ return [
     |
     |
     */
-    'slide1_heading' => 'Welcome!',
-    'slide1_content' => '<p>Thank you for being a part of our community space.</p><p>And <strong>thank you</strong> for being a beta tester!  With your feedback we can help improve the platform for everyone.</p>',
-    'slide2_heading' => 'Your gateway to community repair',
-    'slide2_content' => '<p>In the community space you can:</p> <ul><li>chat with other Restarters</li><li>join repair groups</li><li>share your repair knowledge</li><li>create and RSVP to events</li><li>log the devices you fix at events</li><li>help bring down the barriers to repair</li><p></p></ul>',
-    'slide3_heading' => 'Let\'s get started!',
-    'slide3_content' => '<p>Your starting point is the <strong>community dashboard</strong>.</p><p>Your dashboard gives you at a glance info on the latest happenings in the community, as well as quick access to common actions.</p><p>A good place to start is the <strong>Getting Started</strong> section.</p>',
-    'finishing_action' => 'To the dashboard',
-    'previous' => 'Previous',
-    'next' => 'Next'
+
+    'slide1_heading' => 'Bienvenue!',
+    'slide1_content' => '<p>Merci de faire partie de notre espace communautaire. </p><p>Et <strong>merci</strong> d\'être un bêta testeur!  Avec vos commentaires, nous pouvons aider à améliorer la plate‐ forme pour tout le monde.</p>',
+    'slide2_heading' => 'Votre porte d’entrée vers la réparation communautaire',
+    'slide2_content' => '<p>Dans l\'espace communautaire, vous pouvez:</p> <ul><li>discuter avec d\'autres Restarters</li><li>rejoindre des groupes de réparation</li><li>partager vos connaissances de réparation</li><li>créer et confirmer votre présence à des événements</li><li>inscrire les appareils que vous réparez lors des événements</li><li>aider à faire tomber les barrières à la réparation</li><p></p></ul>',
+    'slide3_heading' => 'C’est parti !',
+    'slide3_content' => '<p>Voici votre point de départ : le <strong>tableau de bord de la communauté</strong>.</p><p>Grâce au tableau de bord, vous avez en un coup d\'œil des informations sur les derniers événements de la communauté, ainsi qu\'un accès rapide aux principales actions.</p><p>Consultez en tout premier lieu la section <strong>Pour commencer</strong>.</p>',
+    'finishing_action' => 'Aller au tableau de bord',
+    'previous' => 'Précédent',
+    'next' => 'Suivant'
 
 ];
--- resources/lang/fr/partials.php
+++ resources/lang/fr/partials.php
@@ -8,28 +8,33 @@ return [
     |--------------------------------------------------------------------------
     */
 
-    'community_news' => 'Community news',
-    'community_news_text' => 'The latest from our community blog - we are always looking for guest posts, send ideas to <a href="mailto:janet@therestartproject.org">janet@therestartproject.org</a>',
-    'see_more_posts' => 'See more posts',
-    'see_all_events' => 'See all events',
+    'community_news' => 'Nouvelles de la communauté',
+    'community_news_text' => 'Les dernières nouvelles de notre blog communautaire ‐ nous sommes constamment à la recherche de sujets, envoyez vos idées à <a href="mailto:janet@therestartproject.org">janet@therestartproject.org</a>',
+    'see_more_posts' => 'Lire d’autres articles',
+    'see_all_events' => 'Voir tous les événements',
+
     'create_an_event' => 'Create an event',
     'create_a_new_event' => 'Create a new event',
     'create_a_new_event_text' => 'Create an event here to publicise it to our network, and to invite volunteers. Afterwards, you can log repairs and discover its impact.',
     'discussion' => 'Discussion',
-    'discussion_text' => 'Meet other event organisers and repair volunteers, trade tips, get help, or discuss the system-level barriers to longer-lasting products',
-    'join_the_discussion' => 'Join the discussion',
+
+    'discussion_text' => 'Rencontrez d\'autres organisateurs d\'événements et des bénévoles réparateurs, échangez des astuces, obtenez de l\'aide ou discutez des barrières systémiques pour des produits plus durables.',
+    'join_the_discussion' => 'Rejoignez la discussion',
+
     'add_device' => 'Add device',
     'how_to_set_up_a_group', 'How to set up a group',
     'how_to_set_up_a_group_text', 'Once you\'ve gone through some preliminary steps to organise a group, we can create one for you here.',
     'read_more' => 'Read more',
-    'how_to_host_an_event' => 'How to host an event',
-    'how_to_host_an_event_text' => 'We have years of experience hosting community repair events, and we offer materials on how to organise events them. We also offer peer to peer support on our forum.',
-    'view_the_materials' => 'view_the_materials',
-    'our_global_environmental_impact' => 'Our Global Environmental Impact',
-    'the_impact_of_restarters' => 'The impact of Restarters\' fixing around the world.',
-    'waste_prevented' => 'waste prevented',
-    'co2' => 'CO<sub>2</sub> emissions prevented',
-    'more_info' => 'More Info',
+
+    'how_to_host_an_event' => 'Comment organiser un événement',
+    'how_to_host_an_event_text' => 'Forts de plusieurs années d\'expérience, nous vous offrons la recette de l\'organisation d\'événements communautaires de réparation. Découvrez également notre forum pour le soutien entre pairs.',
+    'view_the_materials' => 'Voir les ressources',
+    'our_global_environmental_impact' => 'Notre impact environnemental mondial',
+    'the_impact_of_restarters' => 'L\'impact des réparations des Restarters autour du monde.',
+    'waste_prevented' => 'Déchets évités',
+    'co2' => 'Émissions de CO<sub>2</sub> évitées',
+    'more_info' => 'Plus d\'informations',
+
     'restarters_in_your_area' => 'Restarters in your area',
     'information_up_to_date' => 'Keep your group information up to date!',
     'information_up_to_date_text' => 'A fresh profile helps recruit more volunteers. Make sure to link to your website or social media channels.',
@@ -40,26 +45,33 @@ return [
     'none_of_the_above' => 'No of the above',
     'area_text_1' => 'Through this community, potential volunteers self-register and share their location. The platform is designed to connect organisers and fixers.',
     'area_text_2' => 'Through this community, potential volunteers self-register and share their location. Here\'s a list of potential volunteers near you',
-    'upcoming_events' => 'Upcoming events',
-    'no_upcoming_events' => 'No Upcoming Events',
+
+    'wiki_title' => 'Restarters Wiki',
+
+    'upcoming_events' => 'Évènements à venir',
+    'no_upcoming_events' => 'Aucun événement à venir',
     'upcoming_events_text' => 'If possible, the best way to get involved is to go to an event, see how it works, and participate',
-    'upcoming_events_text_2' => 'Find and attend events.  If you have entered your town/city in your profile, you will see the events nearest to you.',
-    'host_getting_started_title' => 'Getting started in community repair',
-    'host_getting_started_text' => 'We offer materials on how to help organise repair events in your community.',
-    'getting_started_link' => 'View the materials',
-    'restarter_getting_started_title' => 'Getting started in community repair',
-    'restarter_getting_started_text' => 'We offer materials on how to help others fix their electronics at community events: on how to share your repair skills in your community and help organise events.',
+    'upcoming_events_text_2' => 'Trouvez et assistez à des événements. Si vous avez entré votre ville dans votre profil, vous verrez les événements les plus proches de vous.',
+    'host_getting_started_title' => 'Pour débuter dans la réparation communautaire',
+    'host_getting_started_text' => 'Comment s’impliquer dans l’organisation d’événements communautaires de réparation.',
+    'getting_started_link' => 'Voir les ressources',
+    'restarter_getting_started_title' => 'Pour débuter dans la réparation communautaire',
+    'restarter_getting_started_text' => 'Comment s’impliquer dans l’organisation d’événements communautaires de réparation.',
     'welcome_title' => 'Getting started in community repair',
     'welcome_text' => 'We offer materials on how to help others fix their electronics at community events: on how to share your repair skills in your community and help organise events',
-    'wiki_title' => 'Restarters Wiki',
-    'wiki_text' => 'A changing selection of pages from our repair wiki.  Read and contribute advice for community repair!',
+   
+    'wiki_text' => 'Une sélection changeante de pages de notre wiki de réparation. Apprenez‐en sur la réparation communautaire et contribuez avec vos propres conseils!',
+
     'remove_volunteer' => 'Remove volunteer',
     'host' => 'Host',
     'update' => 'Update',
     'category' => 'Category',
     'category_none' => 'None of the above',
     'brand' => 'Brand',
+
     'status' => 'Repair status',
+
+
     'fixed' => 'Fixed',
     'model' => 'Model',
     'age' => 'Age',
@@ -67,15 +79,19 @@ return [
     'spare_parts' => 'Spare parts',
     'repairable' => 'Repairable',
     'end' => 'End',
+
     'end_of_life' => 'End-of-life',
     'repair_details' => 'Next steps',
+
     'more_time' => 'More time needed',
     'professional_help' => 'Professional help',
     'diy' => 'Do it yourself',
     'yes' => 'Yes',
+
     'yes_manufacturer' => 'Yes - from manufacturer',
     'yes_third_party' => 'Yes - from 3rd party',
     'no' => 'Not needed',
+
     'n_a' => 'N/A',
     'least_repaired' => 'Least repaired',
     'most_repaired' => 'Most repaired',
@@ -83,7 +99,9 @@ return [
     'description_of_problem' => 'Description of problem',
     'description_of_problem_solution' => 'Description of problem/solution',
     'solution_text' => 'Could the solution comments help Restarters working on a similar device in future?  Or is it a fun case study?',
-    'device_images' => 'Device images',
+
+    'device_images' => 'Device Images',
+
     'remove_file' => 'Remove file',
     'add_image' => 'Add image',
     'no_devices_added' => 'No devices added',
@@ -91,6 +109,7 @@ return [
     'add_a_device' => 'Add a device',
     'this_event_hasnt_started' => 'This event hasn\'t started',
     'event_requires_moderation_by_an_admin' => 'Event requires moderation by an admin',
-    'save' => 'Save device',
 
+    'save' => 'Save device',
+    
 ];
