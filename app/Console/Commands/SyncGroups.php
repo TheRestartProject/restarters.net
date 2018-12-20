946ae62d8d16e13cefbe9538a3de2f12301906b3
Neil Mather <ngmather@gmail.com>
2018-12-20 12:07:02 +0000

    Artisan command for bulk sync of group data from restarters to Wordpress

---
+++ app/Console/Commands/SyncGroups.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Group;
+use Illuminate\Console\Command;
+
+class SyncGroups extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sync:groups';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Performs a bulk sync of group details from restarters application to Wordpress front-end site.  Not designed to be run on a regular basis - sometimes needed for one-off bulk syncs if new fields are introduced to groups in restarters that need uploading to wordpress.';
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $wordpressClient;
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->wordpressClient = new \HieuLe\WordpressXmlrpcClient\WordpressClient();
+        $this->wordpressClient->setCredentials(env('WP_XMLRPC_ENDPOINT'), env('WP_XMLRPC_USER'), env('WP_XMLRPC_PSWD'));
+
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $groups = Group::whereNotNull('wordpress_post_id')->where('wordpress_post_id', '<>', 99999)->get();
+
+        foreach ($groups as $group)
+        {
+            try {
+                $custom_fields = array(
+                    array('key' => 'group_city', 'value' => $group->area),
+                    array('key' => 'group_country', 'value' => $group->country),
+                    array('key' => 'group_website', 'value' => $group->website),
+                    array('key' => 'group_hash', 'value' => $group->idgroups),
+                    array('key' => 'group_latitude', 'value' => $group->latitude),
+                    array('key' => 'group_longitude', 'value' => $group->longitude),
+                );
+
+                $content = array(
+                    'post_type' => 'group',
+                    'post_title' => $group->name,
+                    'post_content' => $group->free_text,
+                    'custom_fields' => $custom_fields
+                );
+
+                if (!empty($group->wordpress_post_id)) {
+                    // We need to remap all custom fields because they all get unique IDs across all posts, so they don't get mixed up.
+                    $existingPost = $this->wordpressClient->getPost($group->wordpress_post_id);
+
+                    foreach ($existingPost['custom_fields'] as $i => $field) {
+                        foreach ($custom_fields as $k => $set_field) {
+                            if ($field['key'] == $set_field['key']) {
+                                $custom_fields[$k]['id'] = $field['id'];
+                            }
+                        }
+                    }
+
+                    $content['custom_fields'] = $custom_fields;
+                    $this->wordpressClient->editPost($group->wordpress_post_id, $content);
+
+                    $this->info('Synced '.$group->name.' (idgroups = '.$group->idgroups.', wordpress_post_id = ' . $group->wordpress_post_id . ')');
+                } else {
+                    $wpid = $this->wordpressClient->newPost($group->name, $group->free_text, $content);
+                    $group->wordpress_post_id = $wpid;
+                    $group->save();
+                }
+            } catch (\Exception $e) {
+                $this->error('Error: '.$group->name.': '.$e->getMessage());
+            }
+        }
+    }
+}
