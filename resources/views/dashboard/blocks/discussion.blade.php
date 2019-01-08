c5860b30124df99d73d07251bdc70dcb4bc2bb0e
Neil Mather <ngmather@gmail.com>
2019-01-08 17:43:21 +0000

    Remove November community updates from discussion dashboard block

--- resources/views/dashboard/blocks/discussion.blade.php
+++ resources/views/dashboard/blocks/discussion.blade.php
@@ -7,9 +7,6 @@
     </div>
     <div class="dashboard__block__content">
         <p>@lang('partials.discussion_text')</p>
-        <div class="dashboard__links dashboard__links__new d-flex flex-row justify-content-end">
-            <a href="https://talk.restarters.net/t/community-update-november-2018-fixometer-updates-roundup/786" target="_blank" rel="noopener noreferrer"><strong>NEW</strong> Community Update: Nov 2018</a>
-        </div>
         <div class="dashboard__links d-flex flex-row justify-content-end">
             <a href="https://talk.restarters.net/t/welcome-to-the-restarters-community/8" target="_blank" rel="noopener noreferrer">Welcome to the Restarters Community</a>
         </div>
