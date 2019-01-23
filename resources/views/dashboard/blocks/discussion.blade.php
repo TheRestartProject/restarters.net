473c470d7f035c0cba707229948425506a64c75a
Neil Mather <ngmather@gmail.com>
2019-01-23 14:38:03 +0000

    Add January community update to topics on discussion block

--- resources/views/dashboard/blocks/discussion.blade.php
+++ resources/views/dashboard/blocks/discussion.blade.php
@@ -7,11 +7,14 @@
     </div>
     <div class="dashboard__block__content">
         <p>@lang('partials.discussion_text')</p>
-        <div class="dashboard__links d-flex flex-row justify-content-end">
-            <a href="https://talk.restarters.net/session/sso?return_path=https://talk.restarters.net/t/welcome-to-the-restarters-community/8" target="_blank" rel="noopener noreferrer">Welcome to the Restarters Community</a>
-        </div>
-        <div class="dashboard__links d-flex flex-row justify-content-end">
-            <a href="https://talk.restarters.net/session/sso?return_path=https://talk.restarters.net/t/introduce-yourself-here/44" target="_blank" rel="noopener noreferrer">Who is everyone?  Meet and greet</a>
+            <div class="dashboard__links d-flex flex-row justify-content-end">
+                <a href="https://talk.restarters.net/session/sso?return_path=https://talk.restarters.net/t/community-update-january-2019/943" target="_blank" rel="noopener noreferrer"><span class="badge badge-danger">NEW!</span> Community Update, January 2019</a>
+            </div>
+            <div class="dashboard__links d-flex flex-row justify-content-end">
+                <a href="https://talk.restarters.net/session/sso?return_path=https://talk.restarters.net/t/welcome-to-the-restarters-community/8" target="_blank" rel="noopener noreferrer">Welcome to the Restarters Community</a>
+            </div>
+            <div class="dashboard__links d-flex flex-row justify-content-end">
+                <a href="https://talk.restarters.net/session/sso?return_path=https://talk.restarters.net/t/introduce-yourself-here/44" target="_blank" rel="noopener noreferrer">Who is everyone?  Meet and greet</a>
         </div>
     </div>
 </section>
