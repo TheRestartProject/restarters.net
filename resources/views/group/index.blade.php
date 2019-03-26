04b997397d9c69ca4d606748c7f22a03a6fc81de
Chris Kelker <chris@wecreatedigital.co.uk>
2019-03-26 09:38:48 +0000

    FIX: Removed sorting on "Groups nearest to you" section

--- resources/views/group/index.blade.php
+++ resources/views/group/index.blade.php
@@ -152,55 +152,45 @@
         @endif
 
         @if( is_null($groups) )
-          <form id="device-search" action="/group/search/column" method="get">
-          <input type="hidden" name="sort_direction"  value="{{$sort_direction}} " class="sr-only">
-          <input type="radio" name="sort_column"   value="name" id="label-name" class="sr-only">
-          <input type="radio" name="sort_column"  value="distance" id="label-location" class="sr-only">
-          <input type="radio" name="sort_column"   value="hosts" id="label-hosts" class="sr-only">
-          <input type="radio" name="sort_column"   value="restarters" id="label-restarters" class="sr-only">
-
           <section class="table-section" id="your-groups">
 
             <h2>@lang('groups.groups_title2') <sup>(<a href="/group/all">See all groups</a>)</sup></h2>
 
             <div class="table-responsive">
-            <table role="table" class="table table-striped table-hover">
+              <table role="table" class="table table-striped table-hover">
                 <thead>
-                <tr>
-                <th width="42"></th>
-                <th width="200" scope="col"><label for="label-name" class="sort-column  @if( $sort_direction == 'ASC' && $sort_column == 'name' ) sort-column-asc @endif" >@lang('groups.groups_name')</label></th>
-                <th width="200" scope="col"><label for="label-location" class="sort-column  @if( $sort_direction == 'ASC' && $sort_column == 'distance' ) sort-column-asc @endif">@lang('groups.groups_location')</label></th>
-                <th width="75" scope="col" class="text-center"><label for="label-hosts" class="sort-column  @if( $sort_direction == 'ASC' && $sort_column == 'hosts' ) sort-column-asc @endif">@lang('groups.groups_hosts')</label></th>
-                <th width="75" scope="col" class="text-center"><label for="label-restarters" class="sort-column  @if( $sort_direction == 'ASC' && $sort_column == 'restarters' ) sort-column-asc @endif">@lang('groups.groups_restarters')</label></th>
-                </tr>
-                </thead>
-              <tbody>
-                @if( !is_null($groups_near_you) && count($groups_near_you) > 0 )
-                  @foreach ($groups_near_you as $group)
-
-                    @include('partials.tables.row-groups')
-
-                  @endforeach
-                @else
                   <tr>
-                    <td colspan="13" align="center" class="p-3">
-                        @if ($your_area)
-                      There are currently no groups near to your area ({{ $your_area }}). Would you consider starting a group?
-                      <br><a href="/group/create/">@lang('groups.create_groups')</a>
-                        @else
-                      You do not currently have a town/city set.  You can set one in <a href="/profile/edit/{{ Auth::user()->id }}">your profile</a>.
-                        @endif
-                    </td>
-                  </tr>
+                    <th width="42"></th>
+                    <th width="200" scope="col"><label for="label-name">@lang('groups.groups_name')</label></th>
+                      <th width="200" scope="col"><label for="label-location">@lang('groups.groups_location')</label></th>
+                        <th width="75" scope="col" class="text-center"><label for="label-hosts">@lang('groups.groups_hosts')</label></th>
+                          <th width="75" scope="col" class="text-center"><label for="label-restarters">@lang('groups.groups_restarters')</label></th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          @if( !is_null($groups_near_you) && count($groups_near_you) > 0 )
+                            @foreach ($groups_near_you as $group)
+
+                              @include('partials.tables.row-groups')
+
+                            @endforeach
+                          @else
+                            <tr>
+                              <td colspan="13" align="center" class="p-3">
+                                @if ($your_area)
+                                  There are currently no groups near to your area ({{ $your_area }}). Would you consider starting a group?
+                                  <br><a href="/group/create/">@lang('groups.create_groups')</a>
+                                @else
+                                  You do not currently have a town/city set.  You can set one in <a href="/profile/edit/{{ Auth::user()->id }}">your profile</a>.
+                                @endif
+                              </td>
+                            </tr>
+                          @endif
+                        </tbody>
+                      </table>
+                    </div>
+                  </section>
                 @endif
-              </tbody>
-            </table>
-
-            </div>
-
-          </section>
-      </form>
-        @endif
 
         @if( !is_null($groups) )
             <form id="device-search" action="/group/all/sort_column/column" method="get">
