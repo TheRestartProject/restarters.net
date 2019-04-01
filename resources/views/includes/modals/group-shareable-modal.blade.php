1b7591e7bd216aca071125637ee172b0607f1854
Chris Kelker <chris@wecreatedigital.co.uk>
2019-04-01 13:17:20 +0100

    FIX: Removed unncessary style classes

--- resources/views/includes/modals/group-shareable-modal.blade.php
+++ resources/views/includes/modals/group-shareable-modal.blade.php
@@ -4,7 +4,7 @@
     <div class="modal-content">
       <div class="modal-header d-flex justify-content-between">
         <h5 id="inviteToGroupShareableLabel">@lang('groups.invite_group_header_email')</h5>
-        <a href="#invite-to-group" data-toggle="modal" data-dismiss="modal" class="align-items-center"><svg width="22" height="13" viewBox="0 0 580 680" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:1.41421;">
+        <a href="#invite-to-group" data-toggle="modal" data-dismiss="modal" class="align-items-center text-dark"><svg width="22" height="13" viewBox="0 0 580 680" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:1.41421;">
             <g transform="matrix(1.47821,0,0,1.39922,-170.205,256.927)">
                 <g transform="matrix(1,0,0,1,0,-233.234)">
                     <g transform="matrix(1,0,0,1,-5.32682,12.0794)">
@@ -40,7 +40,7 @@
           <small class="after-offset">@lang('groups.type_shareable_link_message')</small>
 
           <div class="d-flex flex-row justify-content-between align-items-center">
-            <a href="#" class="close-invite-modal mb-0" data-dismiss="modal">@lang('groups.cancel_invites_link')</a>
+            <a href="#" class="text-dark mb-0" data-dismiss="modal">@lang('groups.cancel_invites_link')</a>
             <button type="submit" class="btn btn-primary m-0" data-dismiss="modal">@lang('groups.done_button')</button>
           </div>
         </form>
