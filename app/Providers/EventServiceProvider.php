76c1ae54eb9fd8672a469e5dafa033086c66d794
Sean <sean@singularitystudios.com>
2019-06-04 13:31:43 -0400

    New notification types added (AdminUserDeleted and AdminModerateEventPhotos) [#111, #112]

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Events\EventImagesUploaded;
 use App\Events\UserDeleted;
 use App\Listeners\RemoveSoftDeletedUserFromAllGroups;
+use App\Listeners\SendAdminModerateEventPhotosNotification;
 use App\Listeners\SendAdminUserDeletedNotification;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -45,6 +47,10 @@ class EventServiceProvider extends ServiceProvider
             RemoveSoftDeletedUserFromAllGroups::class,
             SendAdminUserDeletedNotification::class,
         ],
+
+        EventImagesUploaded::class => [
+            SendAdminModerateEventPhotosNotification::class,
+        ]
     ];
 
     /**
