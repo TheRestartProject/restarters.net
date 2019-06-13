15ec062f9af0558c52221479509edb3a7692832a
WCD <wcd-001@WCDs-MBP.lan>
2019-06-13 09:17:54 +0100

    Feedback: Include date of upcoming event in the same cell as the link

--- resources/views/partials/tables/row-groups.blade.php
+++ resources/views/partials/tables/row-groups.blade.php
@@ -1,5 +1,5 @@
 <tr>
-  <td class="table-cell-icon">
+  <td class="table-cell-icon" colspan="1">
   @php( $group_image = $group->groupImage )
   @if( is_object($group_image) && is_object($group_image->image) )
     <img src="{{ asset('/uploads/thumbnail_' . $group_image->image->path) }}" alt="{{{ $group->name }}}">
@@ -7,23 +7,28 @@
     <img src="{{ asset('/images/placeholder-avatar.png') }}" alt="{{{ $group->name }}}">
   @endif
   </td>
-  <td><a href="/group/view/{{{ $group->idgroups }}}" title="edit group">{{{ $group->name }}}</a></td>
-  <td>{{{ $group->getLocation() }}}</td>
-  <td class="text-center">
+  <td colspan="1"><a href="/group/view/{{{ $group->idgroups }}}" title="edit group">{{{ $group->name }}}</a></td>
+  <td colspan="1">{{{ $group->getLocation() }}}</td>
+  <td class="text-center" colspan="1">
     @php ($next_upcoming_event = $group->getNextUpcomingEvent())
     @if (is_null($next_upcoming_event))
       <p>None planned</p>
     @else
-      <a href="/party/view/{{ $next_upcoming_event->idevents }}">{{ $next_upcoming_event->getEventName() }}</a>
+      <a href="/party/view/{{ $next_upcoming_event->idevents }}">
+        {{ $next_upcoming_event->getEventName() }}
+      </a>
+
+      <div>{{ $next_upcoming_event->getEventDate('D jS M Y') }}</div>
+      <div>{{ $next_upcoming_event->getEventStartEnd() }}</div>
     @endif
   </td>
-  <td class="text-center">
+  <td class="text-center" colspan="1">
     @if ( ! in_array($group->idgroups, $your_groups_uniques) )
       <a class="btn btn-primary" href="/group/join/{{ $group->idgroups }}" id="join-group">Follow</a>
     @endif
   </td>
   @if(  !is_null($groups) && FixometerHelper::hasRole(Auth::user(), 'Administrator'))
-      <td>
+      <td colspan="1">
           {{ \Carbon\Carbon::parse($group->created_at)->diffForHumans() }}
       </td>
   @endif
