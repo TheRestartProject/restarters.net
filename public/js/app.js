8882eaf195621cf1c29bb7f74802526112b9fe3e
WCD <wcd-001@WCDs-MBP.lan>
2019-06-14 10:38:02 +0100

    Feedback: Changed tooltip to popover to keep the same styling as the rest and removed unnecessary extra logic

--- public/js/app.js
+++ public/js/app.js
@@ -78780,13 +78780,10 @@ $(document).ready(function () {
 
     var mins = time[1];
     $('#end-time').val(hours + ':' + mins);
-  }); // Initialize
-  // ---------------------------------------------------------------------
-  // Initalize Tooltips
-
-  $('[data-toggle="tooltip"]').tooltip(); // Copy to clipboard
+  }); // Copy to clipboard
   // Grab any text in the attribute 'data-copy' and pass it to the
   // copy function
+  // ---------------------------------------------------------------------
 
   $('.js-copy').click(function () {
     var text = $(this).attr('data-copy');
@@ -78795,32 +78792,26 @@ $(document).ready(function () {
   });
 }); // COPY TO CLIPBOARD
 // Attempts to use .execCommand('copy') on a created text field
-// Falls back to a selectable alert if not supported
-// Attempts to display status in Bootstrap tooltip
-// ------------------------------------------------------------------------------
+// Copy command
+// boolen if successful then show message
+// After show append original email again
+// Fallback if browser doesn't support .execCommand('copy')
+// ---------------------------------------------------------------------
 
 function copyToClipboard(text, el) {
   var copyTest = document.queryCommandSupported('copy');
-  var elOriginalText = el.attr('data-original-title');
+  var elOriginalText = el.attr('data-original-email');
 
   if (copyTest === true) {
-    var copyTextArea = document.createElement("textarea");
-    copyTextArea.value = text;
-    document.body.appendChild(copyTextArea);
-    copyTextArea.select();
-
     try {
       var successful = document.execCommand('copy');
       var msg = successful ? 'Copied!' : 'Whoops, not copied!';
-      el.attr('data-original-title', msg).tooltip('show');
+      el.attr('data-content', msg).popover('show');
+      el.attr('data-content', elOriginalText);
     } catch (err) {
       console.log('Oops, unable to copy');
     }
-
-    document.body.removeChild(copyTextArea);
-    el.attr('data-original-title', elOriginalText);
   } else {
-    // Fallback if browser doesn't support .execCommand('copy')
     window.prompt("Copy to clipboard: Ctrl+C or Command+C, Enter", text);
   }
 }
--- resources/views/user/all.blade.php
+++ resources/views/user/all.blade.php
@@ -163,7 +163,6 @@
 
                 @if ($display)
                   <tr>
-                      <!-- <td><?php //echo $u->id; ?></td> -->
                       <td>
 
                           @if(FixometerHelper::hasRole($user, 'Administrator'))
@@ -174,10 +173,9 @@
 
                       </td>
                       <td>
-                        <a href="" class="btn-copy js-tooltip js-copy" data-toggle="tooltip" data-placement="top" data-html="true" data-copy="{{ $u->email }}"  title="{{ $u->email }} </br> <b>Copy to clipboard</b>">
+                        <span class="js-copy hover-pointer popover-usergroups" data-toggle="popover" data-trigger="hover" data-placement="top" data-html="true" data-original-email="{{ $u->email }}" data-copy="{{ $u->email }}" data-content="{{ $u->email }} </br> <b>Click/press to copy</b>">
                           {{ str_limit($u->email, 15) }}
-                          {{-- <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path d="M17,9H7V7H17M17,13H7V11H17M14,17H7V15H14M12,3A1,1 0 0,1 13,4A1,1 0 0,1 12,5A1,1 0 0,1 11,4A1,1 0 0,1 12,3M19,3H14.82C14.4,1.84 13.3,1 12,1C10.7,1 9.6,1.84 9.18,3H5A2,2 0 0,0 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5A2,2 0 0,0 19,3Z"></path></svg> --}}
-                        </a>
+                        </span>
                       </td>
                       <td>
                         @if ($u->role == 'Administrator')
