e326d6b6cf2c22a2e646668962be5d1bc6e12f82
Neil Mather <ngmather@gmail.com>
2019-11-29 17:58:55 +0000

    Temporary banner for 2020 event creation
    And change styling of banner.

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -16,6 +16,40 @@
       </div>
   </div>
 
+  {{-- temporary banner --}}
+  <div class="row row-compressed">
+      <div class="col">
+          @if ( is_null(Cookie::get("information-alert-dismissed-createevents2020")) && Auth::check() )
+
+              <div class="alert alert-secondary information-alert alert-dismissible fade show " role="alert" id="usersurvey">
+                <div class="d-sm-flex flex-row justify-content-between align-items-center">
+                  <div class="action-text-left float-left d-flex flex-row">
+                      <span class="icon my-auto d-none">@include('partials.svg-icons.calendar-icon-lg')</span>
+                      <div class="action-text mb-0">
+                          <div class='mb-2'>
+                            <span class='badge badge-success'>NEW!</span>
+                            <strong>Announce events for early 2020</strong>
+                          </div>
+                          <p>We're in demand in January and February - so let's list events for these months now!</p>
+                      </div>
+                  </div>
+
+                  <div class="float-right mt-3 mt-sm-0">
+                      @php( $user = Auth::user() )
+                      <a href='/party/create' class='btn btn-md btn-primary btn-block' title=''>Add events</a>
+                      <button type="button" class="close set-dismissed-cookie float-none ml-2" data-dismiss="alert" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                      </button>
+                  </div>
+                </div>
+              </div>
+
+          @endif
+
+      </div>
+  </div>
+  {{-- temporary banner --}}
+
   <div class="row row-compressed">
       @if ($show_getting_started)
           @include('dashboard.blocks.getting-started')
