e3b78489b964b6a5c86182931ceed9996a3ff3a9
Neil Mather <ngmather@gmail.com>
2019-12-10 14:50:51 +0000

    Refactor user group association trigger

--- app/Http/Controllers/API/UserGroupsController.php
+++ app/Http/Controllers/API/UserGroupsController.php
@@ -16,35 +16,53 @@ class UserGroupsController extends Controller
      * Created as a trigger for Zapier.
      *
      * Only confirmed group memberships - pending invitations not pulled in.
+     *
+     * Only Administrators allowed to access this endpoint.
      */
     public static function changes()
     {
-        $user = Auth::user();
-        if ( ! $user->hasRole('Administrator')) {
+        $authenticatedUser = Auth::user();
+        if ( ! $authenticatedUser->hasRole('Administrator')) {
             return abort(403, 'The authenticated user is not authorized to access this resource');
         }
 
-        $userGroupAudits = \OwenIt\Auditing\Models\Audit::where('auditable_type', 'App\\UserGroups')->groupBy('event', 'created_at')->orderBy('created_at', 'desc')->get();
+        $userGroupAudits = self::getUserGroupAudits();
 
         $userGroupChanges = [];
         foreach ($userGroupAudits as $audit) {
             $userGroupAssociation = UserGroups::withTrashed()->find($audit->auditable_id);
-            $auditCreatedAtAsString = $audit->created_at->toDateTimeString();
             if ( ! is_null($userGroupAssociation) && $userGroupAssociation->isConfirmed()) {
-                $userGroupAssociation->makeHidden(['role', 'status','user','group','deleted_at']);
-                $userGroupChange = $userGroupAssociation->toArray();
-                $userGroupChange['user_id'] = $userGroupAssociation->user;
-                $userGroupChange['user_email'] = User::find($userGroupAssociation->user)->email;
-                $userGroupChange['group_id'] = $userGroupAssociation->group;
-                $userGroupChange['group_name'] = Group::find($userGroupAssociation->group)->name;
-                $userGroupChange['id'] = md5($userGroupAssociation->id . $auditCreatedAtAsString);
-
-                $userGroupChange['change_type'] = $audit->event;
-                $userGroupChange['change_occurred_at'] = $auditCreatedAtAsString;
-                $userGroupChanges[] = $userGroupChange;
+                $userGroupChanges[] = self::mapDetailsAndAuditToChange($userGroupAssociation, $audit);
             }
         }
 
         return response()->json($userGroupChanges);
+    }
+
+    protected static function getUserGroupAudits()
+    {
+        return \OwenIt\Auditing\Models\Audit::where('auditable_type', 'App\\UserGroups')
+            ->groupBy('event', 'created_at')
+            ->orderBy('created_at', 'desc')
+            ->get();
+    }
+
+    protected static function mapDetailsAndAuditToChange($userGroupAssociation, $audit)
+    {
+        $auditCreatedAtAsString = $audit->created_at->toDateTimeString();
+
+        $userGroupAssociation->makeHidden(['role', 'status','user','group','deleted_at']);
+        $userGroupChange = $userGroupAssociation->toArray();
+
+        $userGroupChange['user_id'] = $userGroupAssociation->user;
+        $userGroupChange['user_email'] = User::find($userGroupAssociation->user)->email;
+        $userGroupChange['group_id'] = $userGroupAssociation->group;
+        $userGroupChange['group_name'] = Group::find($userGroupAssociation->group)->name;
+        $userGroupChange['id'] = md5($userGroupAssociation->id . $auditCreatedAtAsString);
+
+        $userGroupChange['change_type'] = $audit->event;
+        $userGroupChange['change_occurred_at'] = $auditCreatedAtAsString;
+
+        return $userGroupChange;
     }
 }
