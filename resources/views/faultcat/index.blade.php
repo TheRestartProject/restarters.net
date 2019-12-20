b204f9c3720ac51d6a73e751e238c53f8131f1ae
Neil Mather <ngmather@gmail.com>
2019-12-20 16:05:37 +0000

    Move shared CSS into shared location

--- resources/views/faultcat/index.blade.php
+++ resources/views/faultcat/index.blade.php
@@ -2,85 +2,7 @@
 
 @section('extra-css')
 
-    <style>
-     body {
-         text-align: center !important;
-     }
-
-     h1 {
-         font-family: 'Asap' !important;
-         font-weight: bold !important;
-         float: left; /* bootstrap pull-left class working locally but not on server!  falling back on this. */
-     }
-
-     img#faultcat {
-         width: 48px;
-         height: 48px;
-         float: right; /* bootstrap pull-right class working locally but not on server!  falling back on this. */
-     }
-
-
-     .title {
-         font-weight: bold;
-     }
-
-     .is-horizontal-center {
-         justify-content: center;
-     }
-
-     .hide {
-         display: none;
-     }
-
-     .show {
-         display: block;
-     }
-
-     .underline {
-         text-decoration: underline;
-     }
-
-     .options {
-         margin-top: 15px;
-     }
-
-     .problem {
-         font-size: 1rem;
-         background-color: #f5f5f5;
-         border: 5px solid #FFDD57;
-         border-radius: 5px;
-     }
-
-     .tag {
-         margin-bottom: 2px;
-     }
-
-     .faultcat .btn {
-         font-family: 'Open Sans';
-     }
-
-     .btn-fault-option-current {
-         background-color: #bdbdbd !important;
-     }
-
-     #btn-info-open {
-         float:right;
-         cursor:pointer;
-     }
-
-     #btn-translate a {
-         color: white;
-         text-decoration: underline;
-     }
-
-     #Y,
-     #N,
-     #fetch,
-     #change {
-         margin-bottom: 2px;
-     }
-
-    </style>
+    @include('faultcat/shared-css')
 
 @endsection
 
@@ -163,7 +85,7 @@
                         </p>
                     </div>
                     <div class="col-4 col-sm-2">
-                        <button id="btn-translate" class="pull-right btn btn-fault-option btn-sm px-3 py-1">
+                        <button id="btn-translate" class="pull-right btn btn-fault-option btn-md px-3 py-1">
                             <a href="https://translate.google.com/#view=home&op=translate&sl=auto&tl=en&text=<?php echo $fault->translate; ?>" target="_blank">
                                 Translate
                             </a>
@@ -181,7 +103,7 @@
                     <p class="title">Is the fault type <span id="fault-type-cur" class="btn btn-md btn-fault-info"><?php echo $fault->fault_type; ?></span>?</p>
                     <p class="buttons is-centered">
                         <button class="btn btn-md btn-success btn-rounded" id="Y">
-                            <span class="underline">Y</span>es/probably/possibly
+                            <span class="underline">Y</span>es / possibly
                         </button>
                         <button type="submit" name="fetch" id="fetch" class="btn btn-md btn-warning btn-rounded">
                             <span class="">I don't know,&nbsp;<span class="underline">F</span>etch another one</span>
