43f9ea8b95b8a0213df08829ddeab91defeae6ff
Neil Mather <ngmather@gmail.com>
2019-12-22 16:13:16 +0000

    Changes to the copy in the info modal

--- resources/views/faultcat/info-modal.blade.php
+++ resources/views/faultcat/info-modal.blade.php
@@ -9,27 +9,28 @@
                 </button>
             </div>
             <div class="modal-body">
-                <div class="alert alert-secondary" style="font-size:smaller">
                 <p>
-                    “We had a kettle; we let it leak:<br>
-              Our not repairing made it worse.<br>
-              We haven't had any tea for a week...<br>
-              The bottom is out of the Universe.”<br>
-              ― Rudyard Kipling
+                    FaultCat is a web app to help <strong>collaboratively categorise the type of faults in computers brought to community events</strong> such as Restart Parties and Repair Cafes.
                 </p>
-                </div>
-            <p>We'd like to group repairs into streams that can be reported and visualised. We don't need to pinpoint the exact cause of each fault.</p>
-            <p>Read more about
-              <a href="https://therestartproject.org/fixometer/why-we-collect-repair-data/" target="_blank">why we collect data</a>
-              and
-              <a href="https://openrepair.org/data-dives/open-repair-data-fixfest-2019-why-computers-fail/" target="_blank">why computers fail</a>.
-            </p>
-            <p>A variety of languages are present in the database, if your browser does not prompt to translate you can use the <a href="https://translate.google.com/" target="_blank">Google Translate button</a>.</p>
-            <!-- https://translate.google.com/intl/en/about/website/
-            We no longer provide new access to Google Translate's Website Translator. This change does not affect existing use of the Website Translator.
-            We encourage users looking to translate webpages to use browsers that support translation natively.
-            -->
-            <p><a href="https://talk.restarters.net/t/faultcat-repair-data-for-the-many-not-the-few-feedback-please/2213" target="_blank">Tell us what you think of this app</a>.</p>
+                <hr/>
+                <p>
+                    At repair events volunteers collect data from the repairs they attempt. The data is uploaded on Restarters.net and shared as <a target="_blank" href="https://openrepair.org/open-data/downloads">open data</a>.
+                </p>
+                <p>
+                    Starting with two <a target="_blank" href="https://openrepair.org/data-dives/open-repair-data-fixfest-2019-why-computers-fail/">data dives in 2019</a>, volunteers have contributed to an initial categorisation of the data. We now want to confirm it, to group repairs into streams that can be reported and visualised.
+                </p>
+                <p>
+                    This is where you can help, by playing FaultCat!
+                </p>
+
+                <hr/>
+
+                <img src="{{ asset('/images/faultcat/099-smiling-cat-face-with-heart-eyes-64px.svg.png') }}" alt="smiling cat" width="48" height="48" />
+
+                <p>Learn how to play, and give us your feedback, in the <a target="_blank" href="https://talk.restarters.net/t/get-involved-in-repair-data-with-faultcat/2313">FaultCat discussion</a>.
+                </p>
+
+
             </div>
         </div>
     </div>
