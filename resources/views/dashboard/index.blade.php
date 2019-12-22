5e2e5a9e812495736518c5c65d2f8d2332fc366a
Neil Mather <ngmather@gmail.com>
2019-12-22 17:03:43 +0000

    Dashboard banner for FaultCat

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -19,7 +19,7 @@
   {{-- temporary banner --}}
   <div class="row row-compressed">
       <div class="col">
-          @if ( is_null(Cookie::get("information-alert-dismissed-createevents2020")) && Auth::check() )
+          @if ( is_null(Cookie::get("information-alert-dismissed-faultcat")) && Auth::check() )
 
               <div class="alert alert-secondary information-alert alert-dismissible fade show " role="alert" id="createevents2020">
                 <div class="d-sm-flex flex-row justify-content-between align-items-center">
@@ -28,15 +28,16 @@
                       <div class="action-text mb-0">
                           <div class='mb-2'>
                             <span class='badge badge-success'>NEW!</span>
-                            <strong>Announce events for early 2020</strong>
+                            <strong>Get involved with community repair data with FaultCat! 😺</strong>
                           </div>
-                          <p>We're in demand in January and February - so let's list events for these months now!</p>
+                          <p>Help categorise the faults we've seen in computers with a simple online task.  <a href="/faultcat">Play here</a> or <a href="{{{ env('DISCOURSE_URL')}}}/session/sso?return_path={{{ env('DISCOURSE_URL') }}}/t/get-involved-in-repair-data-with-faultcat/2313">learn more</a>.
+                          </p>
                       </div>
                   </div>
 
                   <div class="float-right mt-3 mt-sm-0">
                       @php( $user = Auth::user() )
-                      <a href='/party/create' class='btn btn-md btn-primary btn-block' title=''>Add events</a>
+                      <a href='/faultcat' class='btn btn-md btn-primary btn-block' title=''>Let's play</a>
                       <button type="button" class="close set-dismissed-cookie float-none ml-2" data-dismiss="alert" aria-label="Close">
                           <span aria-hidden="true">&times;</span>
                       </button>
