88df7e83b62f4da22b3521b60a4618f47e7dcb15
Neil Mather <ngmather@gmail.com>
2020-01-06 16:03:45 +0000

    Only show login and join buttons if explicitly asked by child layout

--- resources/views/layouts/header_plain.blade.php
+++ resources/views/layouts/header_plain.blade.php
@@ -62,6 +62,7 @@
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id={{ env('GOOGLE_TAG_MANAGER_ID') }}" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <!-- End Google Tag Manager (noscript) -->
 
+        @if ($show_login_join)
             <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
                 <a class="d-none d-sm-block navbar-brand" role="button" data-toggle="collapse" aria-expanded="false" href="#startMenu" aria-controls="startMenu" aria-label="Toggle start menu">
                     @include('includes/logo')
@@ -75,3 +76,4 @@
             </div>
 
             </nav>
+        @endif
