3a36ce7c4edaaaa3286579a4f3fabc3af2cc2e9f
Zenlan <mszpak@zenlan.com>
2020-03-09 10:28:19 -0400

    Updates Misccat sync command (not yet enabled or scheduled) Removes debug logging from Misccat controller

--- app/Console/Commands/SyncMiscCategories.php
+++ app/Console/Commands/SyncMiscCategories.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Misccat;
 
 class SyncMiscCategories extends Command
 {
--- app/Misccat.php
+++ app/Misccat.php
@@ -63,7 +63,7 @@ LIMIT 1;"
     public function fetchStatus() {
 
         $result = [];
-        
+
         $result['total_devices'] = DB::select("
 SELECT COUNT(DISTINCT d.iddevices) AS total
 FROM devices d
@@ -113,7 +113,7 @@ HAVING
 AND (top_crowd_opinion != 'Misc')
 ) AS results
 ");
-        
+
         $result['list_recats'] = DB::select("
 SELECT
 d.iddevices,
@@ -130,8 +130,8 @@ HAVING
 AND (top_crowd_opinion != 'Misc')
 ORDER BY top_crowd_opinion ASC, all_crowd_opinions_count DESC, d.iddevices DESC
 ");
-  
-                $result['total_splits'] = DB::select("
+
+        $result['total_splits'] = DB::select("
 SELECT COUNT(DISTINCT items) as total FROM
 (SELECT
 o.iddevices AS items,
@@ -144,8 +144,8 @@ HAVING
 (all_crowd_opinions_count = 3 AND top_crowd_opinion_percentage < 40)
 ) AS results
 ");
-        
-                $result['list_splits'] = DB::select("
+
+        $result['list_splits'] = DB::select("
 SELECT
 d.iddevices,
 (SELECT o1.category FROM devices_misc_opinions o1 WHERE o1.iddevices = o.iddevices GROUP BY o1.category ORDER BY COUNT(o1.category) DESC LIMIT 1) AS top_crowd_opinion,
@@ -163,23 +163,23 @@ HAVING
 (all_crowd_opinions_count = 3 AND top_crowd_opinion_percentage < 40)
 ORDER BY all_crowd_opinions_count DESC, d.iddevices DESC
 ");
-        
-                $result['total_eee'] = DB::select("
+
+        $result['total_eee'] = DB::select("
 SELECT COUNT(DISTINCT o.iddevices) AS total
 FROM devices_misc_opinions o
 WHERE o.eee = 1
-");                
-                                $result['total_non_eee'] = DB::select("
+");
+        $result['total_non_eee'] = DB::select("
 SELECT COUNT(DISTINCT o.iddevices) AS total
 FROM devices_misc_opinions o
 WHERE o.eee = 0
 ");
-                                
-                                $result['total_not_sure'] = DB::select("
+
+        $result['total_not_sure'] = DB::select("
 SELECT COUNT(DISTINCT o.iddevices) AS total
 FROM devices_misc_opinions o
 WHERE o.eee = 2
-");                
+");
 
         return $result;
     }
@@ -192,34 +192,46 @@ WHERE o.eee = 2
      */
     public function updateDevices() {
 
-//        DB::statement("CREATE TEMPORARY TABLE IF NOT EXISTS `devices_misc_temporary` AS (
-//SELECT
-//d.iddevices,
-//COALESCE(ANY_VALUE(a.category),(SELECT o1.category FROM devices_misc_opinions o1 WHERE o1.iddevices = o.iddevices GROUP BY o1.category ORDER BY COUNT(o1.category) DESC LIMIT 1)) AS winning_opinion,
-//ANY_VALUE(a.category) AS adjudicated_opinion,
-//ROUND((SELECT COUNT(o2.category) as top_crowd_opinion_count FROM devices_misc_opinions o2 WHERE o2.iddevices = o.iddevices GROUP BY o2.category ORDER BY top_crowd_opinion_count DESC LIMIT 1) /
-//(SELECT COUNT(o2.category) as all_votes FROM devices_misc_opinions o2 WHERE o2.iddevices = o.iddevices) * 100) AS top_crowd_opinion_percentage,
-//COUNT(o.category) AS all_crowd_opinions_count
-//FROM devices d
-//LEFT OUTER JOIN devices_misc_opinions o ON o.iddevices = d.iddevices
-//LEFT OUTER JOIN devices_misc_adjudicated a ON a.iddevices = d.iddevices
-//WHERE d.category IN (46)
-//AND LENGTH(d.problem) > 0
-//GROUP BY d.iddevices
-//HAVING
-//(all_crowd_opinions_count >= 2 AND top_crowd_opinion_percentage >= 60)
-//OR adjudicated_opinion IS NOT NULL
-//ORDER BY NULL
-//        );");
-//
-//        DB::statement("ALTER TABLE `devices_misc_temporary` ADD PRIMARY KEY(`iddevices`);");
-//
-//        $result = DB::update("UPDATE devices d, devices_misc_opinions o 
-//SET d.category = o.category
-//WHERE d.iddevices = o.iddevices;");
-//
-//        DB::statement("DROP TEMPORARY TABLE IF EXISTS `devices_misc_temporary`");
-        $result = 0;
+        return 0;
+        
+        DB::statement("CREATE TEMPORARY TABLE IF NOT EXISTS `devices_misc_temporary` AS (
+SELECT
+d.iddevices,
+COALESCE(ANY_VALUE(a.category),(SELECT o1.category FROM devices_misc_opinions o1 WHERE o1.iddevices = o.iddevices GROUP BY o1.category ORDER BY COUNT(o1.category) DESC LIMIT 1)) AS winning_opinion,
+ANY_VALUE(a.category) AS adjudicated_opinion,
+ROUND((SELECT COUNT(o2.category) as top_crowd_opinion_count FROM devices_misc_opinions o2 WHERE o2.iddevices = o.iddevices GROUP BY o2.category ORDER BY top_crowd_opinion_count DESC LIMIT 1) /
+(SELECT COUNT(o2.category) as all_votes FROM devices_misc_opinions o2 WHERE o2.iddevices = o.iddevices) * 100) AS top_crowd_opinion_percentage,
+COUNT(o.category) AS all_crowd_opinions_count,
+0 AS idcategories
+FROM devices d
+LEFT OUTER JOIN devices_misc_opinions o ON o.iddevices = d.iddevices
+LEFT OUTER JOIN devices_misc_adjudicated a ON a.iddevices = d.iddevices
+WHERE d.category IN (46)
+AND LENGTH(d.problem) > 0
+GROUP BY d.iddevices
+HAVING
+adjudicated_opinion IS NOT NULL
+OR (
+(all_crowd_opinions_count > 1 AND top_crowd_opinion_percentage > 50)
+AND (winning_opinion != 'Misc')
+)
+ORDER BY NULL
+        );");
+
+        DB::statement("ALTER TABLE `devices_misc_temporary` ADD PRIMARY KEY(`iddevices`);");
+
+        DB::update("UPDATE devices_misc_temporary t, categories c 
+SET t.idcategories = c.idcategories
+WHERE t.winning_opinion = c.`name`;
+");
+
+        $result = DB::update("UPDATE devices d, devices_misc_temporary t 
+SET d.category = t.idcategories
+WHERE d.iddevices = t.iddevices;
+");
+
+        DB::statement("DROP TEMPORARY TABLE IF EXISTS `devices_misc_temporary`");
+
         return $result;
     }
 
