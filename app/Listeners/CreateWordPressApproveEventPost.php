24c30b12897263e9cc26d78e961ddb49df7c8f4d
Neil Mather <ngmather@gmail.com>
2020-03-12 18:04:28 +0000

    Code cleanup

--- app/Listeners/CreateWordPressApproveEventPost.php
+++ app/Listeners/CreateWordPressApproveEventPost.php
@@ -14,6 +14,8 @@ use FixometerHelper;
 
 class CreateWordPressApproveEventPost
 {
+    protected $wpClient;
+
     /**
      * Create the event listener.
      *
@@ -21,7 +23,8 @@ class CreateWordPressApproveEventPost
      */
     public function __construct()
     {
-        //
+        $this->wpClient = new \HieuLe\WordpressXmlrpcClient\WordpressClient();
+        $this->wpClient->setCredentials(env('WP_XMLRPC_ENDPOINT'), env('WP_XMLRPC_USER'), env('WP_XMLRPC_PSWD'));
     }
 
     /**
@@ -32,66 +35,53 @@ class CreateWordPressApproveEventPost
      */
     public function handle(ApproveEvent $event)
     {
-
-       // Set event variable
         $id = $event->party->idevents;
         $data = $event->data;
 
-       // Define model
         $theParty = Party::find($id);
 
         if (!empty($theParty)) {
             try {
-                if (( env('APP_ENV') == 'development' || env('APP_ENV') == 'local' ) && isset($data['moderate']) && $data['moderate'] == 'approve') { // For testing purposes
+                // For testing purposes
+                if (( env('APP_ENV') == 'development' || env('APP_ENV') == 'local' ) && isset($data['moderate']) && $data['moderate'] == 'approve') {
                     $theParty->update(['wordpress_post_id' => 99999]);
-                } elseif (( env('APP_ENV') != 'development' && env('APP_ENV') != 'local' ) && isset($data['moderate']) && $data['moderate'] == 'approve') {
+                } elseif (isset($data['moderate']) && $data['moderate'] == 'approve') {
                     $startTimestamp = strtotime($data['event_date'] . ' ' . $data['start']);
                     $endTimestamp = strtotime($data['event_date'] . ' ' . $data['end']);
 
                     $group = Group::where('idgroups', $data['group'])->first();
 
-                  // if(env('APP_ENV') != 'development' && env('APP_ENV') != 'local') {
-                  /** Prepare Custom Fields for WP XML-RPC - get all needed data **/
-                  //$Host = $Groups->findHost($group);
-                    $custom_fields = array(
-                    // array('key' => 'party_host',            'value' => $Host->hostname),
-                    // array('key' => 'party_hostavatarurl',   'value' => env('UPLOADS_URL') . 'mid_' .$Host->path),
-                    array('key' => 'party_grouphash',       'value' => $data['group']),
-                    array('key' => 'party_venue',           'value' => $data['venue']),
-                    array('key' => 'party_location',        'value' => $data['location']),
-                    array('key' => 'party_time',            'value' => $data['start'] . ' - ' . $data['end']),
-                    array('key' => 'party_groupcountry',    'value' => $group->country),
-                    array('key' => 'party_groupcity', 'value' => $group->area),
-                    array('key' => 'party_date',            'value' => $data['event_date']),
-                    array('key' => 'party_timestamp',       'value' => $startTimestamp),
-                    array('key' => 'party_timestamp_end',   'value' => $endTimestamp),
-                    array('key' => 'party_stats',           'value' => $id),
-                    array('key' => 'party_lat',             'value' => $data['latitude']),
-                    array('key' => 'party_lon',             'value' => $data['longitude'])
-                    );
+                    $custom_fields = [
+                        ['key' => 'party_grouphash', 'value' => $data['group']],
+                        ['key' => 'party_venue', 'value' => $data['venue']],
+                        ['key' => 'party_location', 'value' => $data['location']],
+                        ['key' => 'party_time', 'value' => $data['start'] . ' - ' . $data['end']],
+                        ['key' => 'party_groupcountry', 'value' => $group->country],
+                        ['key' => 'party_groupcity', 'value' => $group->area],
+                        ['key' => 'party_date', 'value' => $data['event_date']],
+                        ['key' => 'party_timestamp', 'value' => $startTimestamp],
+                        ['key' => 'party_timestamp_end', 'value' => $endTimestamp],
+                        ['key' => 'party_stats', 'value' => $id],
+                        ['key' => 'party_lat', 'value' => $data['latitude']],
+                        ['key' => 'party_lon', 'value' => $data['longitude']]
+                    ];
 
-                  /** Start WP XML-RPC **/
-                    $wpClient = new \HieuLe\WordpressXmlrpcClient\WordpressClient();
-                    $wpClient->setCredentials(env('WP_XMLRPC_ENDPOINT'), env('WP_XMLRPC_USER'), env('WP_XMLRPC_PSWD'));
-
-                    $content = array(
-                    'post_type' => 'party',
-                    'custom_fields' => $custom_fields
-                    );
+                    $content = [
+                        'post_type' => 'party',
+                        'custom_fields' => $custom_fields
+                    ];
 
                     $party_name = !empty($data['venue']) ? $data['venue'] : $data['location'];
-                    $wpid = $wpClient->newPost($party_name, $data['free_text'], $content);
+                    $wpid = $this->wpClient->newPost($party_name, $data['free_text'], $content);
 
                     $theParty->update(['wordpress_post_id' => $wpid]);
                 }
-
-             // If WordPress message fails then send Notification to Admins with Preference
             } catch (\Exception $e) {
                  Log::error("An error occurred during Wordpress event creation: " . $e->getMessage());
                  $notify_users = FixometerHelper::usersWhoHavePreference('admin-approve-wordpress-event-failure');
                  Notification::send($notify_users, new AdminWordPressCreateEventFailure([
-                 'event_venue' => $theParty->venue,
-                 'event_url' => url('/party/edit/'.$theParty->idevents),
+                    'event_venue' => $theParty->venue,
+                    'event_url' => url('/party/edit/'.$theParty->idevents),
                  ]));
             }
         }
