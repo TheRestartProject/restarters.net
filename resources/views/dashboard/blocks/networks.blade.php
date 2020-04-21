1205c927a2a9416670096d3b1f94e8df3bb042c3
Neil Mather <ngmather@gmail.com>
2020-04-21 17:53:48 +0100

    Lang resources for networks block

--- resources/views/dashboard/blocks/networks.blade.php
+++ resources/views/dashboard/blocks/networks.blade.php
@@ -1,12 +1,12 @@
 <section class="dashboard__block">
 
     <div class="dashboard__block__header dashboard__block__header--wiki">
-        <h4>Your Networks</h4>
+        <h4>@lang('dashboard.your_networks')</h4>
     </div>
 
     <div class="dashboard__block__content dashboard__block__content--table">
 
-        <p>@lang('partials.hot_topics_text')</p>
+        <p>@lang('dashboard.networks_you_coordinate')</p>
             <ul>
                 @php( $count = 1 )
                 @foreach( $user->networks as $network )
