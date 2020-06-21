a9677bb3fe77b867c55c060500dce9efdf3a4472
Neil Mather <ngmather@gmail.com>
2020-06-21 12:44:08 +0100

    fix: formatting of upcoming and past event dashboard blocks

--- resources/views/partials/past.blade.php
+++ resources/views/partials/past.blade.php
@@ -12,8 +12,8 @@
             <thead>
                 <tr>
                     <th scope="col">@lang('events.event_name')</th>
-                    <th scope="col" class="cell-date d-none d-sm-block">@lang('events.event_date')</th>
-                    <th scope="col" class="cell-locations"></th>
+                    <th scope="col" class="cell-date d-none d-sm-table-cell">@lang('events.event_date')</th>
+                    <th scope="col" class=""></th>
                 </tr>
             </thead>
             <tbody>
@@ -21,7 +21,7 @@
                   @foreach($past_events as $past_event)
                     <tr>
                         <td class="cell-name"><a href="/party/view/{{ $past_event->idevents }}">{{ $past_event->getEventName() }}</a></td>
-                        <td class="d-none d-sm-block">{{ $past_event->getEventDate() }}</td>
+                        <td class="d-none d-sm-table-cell">{{ $past_event->getEventDate() }}</td>
                         @if( $past_event->allDevices->count() == 0 )
                           <td><a href="/party/view/{{{ $past_event->idevents }}}#devices">@lang('dashboard.log_devices')</a></td>
                         @else
--- resources/views/partials/upcoming2.blade.php
+++ resources/views/partials/upcoming2.blade.php
@@ -8,35 +8,25 @@
         <table role="table" class="table table-events table-striped">
             <thead>
                 <tr>
-                    <th scope="col" class="table-cell-icon"></th>
                     <th style="font-family:Asap;font-weight:bold" scope="col">@lang('events.event_name')</th>
                     <th style="font-family:Asap;font-weight:bold" scope="col" class="cell-date">@lang('events.event_date')/@lang('events.event_time')</th>
-                    <th style="font-family:Asap;font-weight:bold" scope="col" class="cell-locations d-none d-sm-block">@lang('events.event_location')</th>
+                    <th style="font-family:Asap;font-weight:bold" scope="col" class="cell d-none d-sm-block">@lang('events.event_location')</th>
                 </tr>
             </thead>
             <tbody>
               @if ( !$upcoming_events->isEmpty() )
                 @foreach($upcoming_events as $event)
                   <tr>
-                      <td class="table-cell-icon">
-                                              @php( $group_image = $event->theGroup->groupImage )
-                          @if( is_object($group_image) && is_object($group_image->image) )
-                              <img style="display:inline-block;" src="{{ asset('/uploads/thumbnail_' . $group_image->image->path) }}" alt="{{{ $event->theGroup->name }}}" title="{{{ $event->theGroup->name }}}" />
-                          @else
-                              <img style="display:inline-block;padding-right:5px" src="{{ asset('/images/placeholder-avatar.png') }}" alt="{{{ $event->theGroup->name }}}">
-                          @endif
-
-                      </td>
                       <td class="cell-name">
                           <a href="/party/view/{{ $event->idevents }}">{{ $event->getEventName() }}</a>
                       </td>
                     <td class="cell-date">{{ $event->getEventDate('D jS M') }}<br>{{ $event->getEventStartEnd() }}</td>
-                    <td class="d-none d-sm-block cell-locations">{{ $event->location }}</td>
+                    <td class="d-none d-sm-block">{{ $event->location }}</td>
                   </tr>
                 @endforeach
               @else
                 <tr>
-                  <td colspan="4" style="text-align: center">@lang('partials.no_upcoming_events')</td>
+                  <td colspan="3" style="text-align: center">@lang('partials.no_upcoming_events')</td>
                 </tr>
               @endif
             </tbody>
