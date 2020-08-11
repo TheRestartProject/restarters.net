2576eb76af593fc3d0b4e7de037bf36661053df5
Edward Hibbert <edward@ehibbert.org.uk>
2020-08-11 18:25:38 +0100

    Remove tests left over from cut and paste

--- tests/Unit/Events/CountsTests.php
+++ tests/Unit/Events/CountsTests.php
@@ -29,53 +29,6 @@ class CountsTests extends TestCase
         DB::statement("SET foreign_key_checks=1");
     }
 
-    /** @test */
-    public function it_can_check_if_coordinator_can_edit_for_network()
-    {
-        // arrange
-        $network = factory(Network::class)->create();
-
-        $group = factory(Group::class)->create();
-        $network->addGroup($group);
-
-        $event = factory(Party::class)->create(['group' => $group]);
-
-        $coordinator1 = factory(User::class)->state('NetworkCoordinator')->create();
-        $network->addCoordinator($coordinator1);
-        $coordinator2 = factory(User::class)->state('NetworkCoordinator')->create();
-        $host = factory(User::class)->state('Host')->create();
-
-        // assert
-        $this->actingAs($coordinator1);
-        $this->assertTrue(FixometerHelper::userHasEditPartyPermission($event->idevents));
-
-        $this->actingAs($coordinator2);
-        $this->assertFalse(FixometerHelper::userHasEditPartyPermission($event->idevents));
-
-        $this->actingAs($host);
-        $this->assertFalse(FixometerHelper::userHasEditPartyPermission($event->idevents));
-    }
-
-    /** @test */
-    public function it_can_check_if_host_can_edit_for_group()
-    {
-        // arrange
-        $network = factory(Network::class)->create();
-
-        $group = factory(Group::class)->create();
-        $network->addGroup($group);
-
-        $event = factory(Party::class)->create(['group' => $group]);
-
-        $host = factory(User::class)->state('Host')->create();
-        $group->addVolunteer($host);
-        $group->makeMemberAHost($host);
-
-        // assert
-        $this->actingAs($host);
-        $this->assertTrue(FixometerHelper::userHasEditPartyPermission($event->idevents));
-    }
-
     // Event counts should only include events in the past.
     /** @test */
     public function it_can_check_if_event_counts_past()
