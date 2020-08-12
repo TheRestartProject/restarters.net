e185bd2bedc130c34ebfd19ebb6dd2bc4da62377
Edward Hibbert <edward@ehibbert.org.uk>
2020-08-12 09:28:30 +0100

    Test that the devices are fetched.

--- tests/Unit/Events/CountsTests.php
+++ tests/Unit/Events/CountsTests.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use App\Device;
 use App\Group;
 use App\Network;
 use App\Party;
@@ -43,13 +44,30 @@ class CountsTests extends TestCase
             'event_date' => '2130-01-01',
             'start' => '12:13'
         ]);
-        $event->save();
 
-        $pastcount = count($event->ofThisGroup('group', true, false));
-        $allcount =  count($event->ofThisGroup('group', false, false));
+        // Delete all events - can hang around in the DB because the relation doesn't cascade the deletes.
+        DB::delete("DELETE FROM devices WHERE event = {$event->idevents}");
+        $this->assertEquals(0, count($event->devices));
+
+        // Create a device on the event.
+        $device = factory(Device::class)->states('fixed', 'mobile')->create([
+            'event' => $event->idevents,
+        ]);
+
+        // Save and refetch the event to make sure that it looks how we expect.
+        $event->save();
+        $event2 = $event->fresh();
+
+        $pastcount = count($event2->ofThisGroup('group', true, false));
+        $allcount =  count($event2->ofThisGroup('group', false, false));
 
         // The event we created is in the future, so the count of all should be more than the count in the past.
         $this->assertGreaterThan($pastcount, $allcount);
-        $this->assertEquals('2130-01-01 12:13', $event->event_timestamp);
+
+        // The accessor should construct the timestamp.
+        $this->assertEquals('2130-01-01 12:13:00', $event2->event_timestamp);
+
+        // The devices should be fetched on demand.
+        $this->assertEquals(1, count($event2->devices));
     }
 }
