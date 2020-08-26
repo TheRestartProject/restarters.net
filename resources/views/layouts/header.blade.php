d1d196f495726494d5d2c5c22ee2cdf62e62d235
Zenlan <mszpak@zenlan.com>
2020-08-26 10:19:55 -0400

    Fixes MobiFixORA meta title issue

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -8,7 +8,15 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }} @hasSection('title')- @yield('title')@endif</title>
+    <!--<title>{{ config('app.name', 'Laravel') }} @hasSection('title')- @yield('title')@endif</title>-->
+
+    <title>
+        @hasSection('title')
+        @yield('title')
+        @else
+        {{ config('app.name', 'Laravel') }}
+        @endif
+    </title>
 
     <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon" />
 
