782b8cda2f68625118d21a7fd5b3285537896aa1
Edward Hibbert <edward@ehibbert.org.uk>
2020-09-21 10:43:27 +0100

    Assume misc powered items have no weight

--- app/Party.php
+++ app/Party.php
@@ -669,7 +669,7 @@ class Party extends Model implements Auditable
                         break;
                 }
 
-                if ($device->isFixed() && !$device->deviceCategory->weight && !$device->estimate) {
+                if ($device->isFixed() && ($device->category == 46 || !$device->deviceCategory->weight) && !$device->estimate) {
                     $no_weight++;
                 }
             }
