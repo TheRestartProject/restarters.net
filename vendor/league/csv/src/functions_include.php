a68d2dd7ad725d03ffd971da42c51a0505c1a7bc
Edward Hibbert <edward@ehibbert.org.uk>
2020-10-16 07:27:31 +0100

    Remove vendor folder

--- vendor/league/csv/src/functions_include.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// Don't redefine the functions if included multiple times.
-if (!function_exists('League\Csv\bom_match')) {
-    require __DIR__.'/functions.php';
-}
--- vendor/msurguy/honeypot/src/lang/fr/validation.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return array(
-    'honeypot'  => 'Tentative de spam detectée',
-    'honeytime' => 'Veuillez patienter quelques instants avant de soumettre le formulaire à nouveau',
-);
