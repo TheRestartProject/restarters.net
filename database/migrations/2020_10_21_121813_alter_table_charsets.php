add244f4a3e23f28cd56ca0007470b8eda21efd0
Zenlan <mszpak@zenlan.com>
2020-10-22 07:50:39 -0400

    Updates DAT-21 charset/collation migration and test

--- database/migrations/2020_10_21_121813_alter_table_charsets.php
+++ database/migrations/2020_10_21_121813_alter_table_charsets.php
@@ -16,14 +16,19 @@ class AlterTableCharsets extends Migration {
      */
     public function up() {
 
-        $tables = $this->_tableCharsets();
-        foreach ($tables as $table) {
-            if ($table['TABLE_COLLATION'] <> $collate) {
-                $name = $table['TABLE_NAME'];
-                Log::info("ALTER TABLE `$name` CONVERT TO CHARACTER SET $this->charset COLLATE $this->collate");
-                DB::statement("ALTER TABLE `$name` CONVERT TO CHARACTER SET $this->charset COLLATE $this->collate");
-            }
-        }
+        Log::info('START MIGRATE AlterTableCharsets');
+
+        $this->_report('DAT21_', '_a');
+
+        $this->_alterTables();
+
+        $this->_fixData();
+
+        $this->_report('DAT21_', '_b');
+
+        Log::info('== SEE ' . storage_path() . '/logs/DAT21*.log FOR RESULTS ===');
+        Log::info('END MIGRATE AlterTableCharsets');
+
     }
 
     /**
@@ -42,7 +47,6 @@ class AlterTableCharsets extends Migration {
                 } else if ($collate == 'utf8_general_ci') {
                     $charset = 'utf8';
                 }
-                Log::info("ALTER TABLE `$name` CONVERT TO CHARACTER SET $charset COLLATE $collate");
                 DB::statement("ALTER TABLE `$name` CONVERT TO CHARACTER SET $charset COLLATE $collate");
             }
         }
@@ -50,163 +54,147 @@ class AlterTableCharsets extends Migration {
 
     private function _fixData() {
 
-        "UPDATE `groups` SET `name` = REPLACE(`name`, 'Ã©', 'é')";
-        "UPDATE `devices` SET `brand` = REPLACE(`brand`, 'Ã©', 'é')";
-        "UPDATE `devices` SET `model` = REPLACE(`model`, 'Ã©', 'é')";
-        "UPDATE `devices` SET `problem` = REPLACE(`problem`, 'Ã©', 'é')";
+        $tables = $this->_dataTables();
 
-        "UPDATE `groups` SET `name` = REPLACE(`name`, 'Ã‰', 'É')";
-        "UPDATE `devices` SET `brand` = REPLACE(`brand`, 'Ã‰', 'É')";
-        "UPDATE `devices` SET `model` = REPLACE(`model`, 'Ã‰', 'É')";
-        "UPDATE `devices` SET `problem` = REPLACE(`problem`, 'Ã‰', 'É')";
+        $chars = [
+            ['â€™', '\''],
+            ['â€ž', '\"'],
+            ['â€œ', '\"'],
+            ['â€', '\"'],
+            ['â€“', '-'],
+            ['â‚¬', '€'],
+            ['Ã©', 'é'],
+            ['Ã‰', 'É'],
+            ['Ã¨', 'è'],
+            ['Ã§', 'ç'],
+            ['Ãª', 'ê'],
+            ['Ã¢', 'â'],
+            ['Ã¥', 'å'],
+            ['Ã±', 'ñ'],
+            ['Ã³', 'ó'],
+            ['Ã¼', 'ü'],
+            ['Ã»', 'û'],
+            ['Ã¶', 'ö'],
+            ['Ã–', 'Ö'],
+            ['Ã¤', 'ä'],
+            ['ÃŸ', 'ß'],
+            ['Â£', '£'],
+            ['Ã«', 'ë'],
+            ['Ã´', 'ô'],
+            ['Ã', 'à'],
+        ];
 
-        "UPDATE `groups` SET `name` = REPLACE(`name`, 'Ã¨', 'è')";
-        "UPDATE `devices` SET `brand` = REPLACE(`brand`, 'Ã¨', 'è')";
-        "UPDATE `devices` SET `model` = REPLACE(`model`, 'Ã¨', 'è')";
-        "UPDATE `devices` SET `problem` = REPLACE(`problem`, 'Ã¨', 'è')";
+        $format = 'UPDATE `%1$s` SET `%2$s` = REPLACE(`%2$s`, "%3$s", "%4$s")';
 
-        "UPDATE `groups` SET `name` = REPLACE(`name`, 'Ã§', 'ç')";
-        "UPDATE `devices` SET `brand` = REPLACE(`brand`, 'Ã§', 'ç')";
-        "UPDATE `devices` SET `model` = REPLACE(`model`, 'Ã§', 'ç')";
-        "UPDATE `devices` SET `problem` = REPLACE(`problem`, 'Ã§', 'ç')";
+        foreach ($tables as $table => $fields) {
+            $key = $fields['key'];
+            foreach ($fields['fields'] as $field) {
+                foreach ($chars as $pair) {
+                    $qry = sprintf($format, $table, $field, $pair[0], $pair[1]);
+                    DB::statement($qry);
+                }
+            }
+        }
 
-        "UPDATE `groups` SET `name` = REPLACE(`name`, 'Ã¥', 'å')";
-        "UPDATE `devices` SET `brand` = REPLACE(`brand`, 'Ã¥', 'å')";
-        "UPDATE `devices` SET `model` = REPLACE(`model`, 'Ã¥', 'å')";
-        "UPDATE `devices` SET `problem` = REPLACE(`problem`, 'Ã¥', 'å')";
+        $qry = 'UPDATE `groups` SET `name` = REPLACE(`name`, "Cafe", "Café")';
+        DB::statement($qry);
+        $qry = 'UPDATE `groups` SET `name` = REPLACE(`name`, "Cafes", "Cafés")';
+        DB::statement($qry);
+        $qry = 'UPDATE `groups` SET `name` = REPLACE(`name`, "cafe", "Café")';
+        DB::statement($qry);
+    }
 
-        "UPDATE `groups` SET `name` = REPLACE(`name`, 'Ã±', 'ñ')";
-        "UPDATE `devices` SET `brand` = REPLACE(`brand`, 'Ã±', 'ñ')";
-        "UPDATE `devices` SET `model` = REPLACE(`model`, 'Ã±', 'ñ')";
-        "UPDATE `devices` SET `problem` = REPLACE(`problem`, 'Ã±', 'ñ')";
+    private function _alterTables() {
+//        transaction appears to break test
+//        DB::beginTransaction();
+        try {
+            $tables = $this->_tableCharsets();
+            foreach ($tables as $table) {
+                if ($table['TABLE_COLLATION'] <> $this->collate) {
+                    $name = $table['TABLE_NAME'];
+                    Log::info("ALTER TABLE `$name` CONVERT TO CHARACTER SET $this->charset COLLATE $this->collate");
+                    DB::statement("ALTER TABLE `$name` CONVERT TO CHARACTER SET $this->charset COLLATE $this->collate");
+                }
+            }
+//            DB::commit();
+        } catch (\Illuminate\Database\QueryException $e) {
+            Log::info($e->getMessage());
+//            DB::rollback();
+        }
+    }
 
-        "UPDATE `groups` SET `name` = REPLACE(`name`, 'Ã³', 'ó')";
-        "UPDATE `devices` SET `brand` = REPLACE(`brand`, 'Ã³', 'ó')";
-        "UPDATE `devices` SET `model` = REPLACE(`model`, 'Ã³', 'ó')";
-        "UPDATE `devices` SET `problem` = REPLACE(`problem`, 'Ã³', 'ó')";
+    private function _dataTables() {
+        return [
+            'groups' => [
+                'key' => 'idgroups',
+                'fields' => [
+                    'name',
+                    'location',
+                    'free_text',
+                ],
+            ],
+            'devices' => [
+                'key' => 'iddevices',
+                'fields' => [
+                    'brand',
+                    'model',
+                    'problem',
+                    'notes',
+                    'item_type',
+                ],
+            ],
+            'events' => [
+                'key' => 'idevents',
+                'fields' => [
+                    'location',
+                    'venue',
+                    'free_text',
+                ],
+            ],
+            'networks' => [
+                'key' => 'id',
+                'fields' => [
+                    'name',
+                    'description',
+                    'shortname',
+                ],
+            ],
+            'users' => [
+                'key' => 'id',
+                'fields' => [
+                    'name',
+                    'username',
+                ],
+            ],
+        ];
+    }
 
-        "UPDATE `groups` SET `name` = REPLACE(`name`, 'Ã¼', 'ü')";
-        "UPDATE `devices` SET `brand` = REPLACE(`brand`, 'Ã¼', 'ü')";
-        "UPDATE `devices` SET `model` = REPLACE(`model`, 'Ã¼', 'ü')";
-        "UPDATE `devices` SET `problem` = REPLACE(`problem`, 'Ã¼', 'ü')";
-
-        "UPDATE `groups` SET `name` = REPLACE(`name`, 'Ã¶', 'ö')";
-        "UPDATE `devices` SET `brand` = REPLACE(`brand`, 'Ã¶', 'ö')";
-        "UPDATE `devices` SET `model` = REPLACE(`model`, 'Ã¶', 'ö')";
-        "UPDATE `devices` SET `problem` = REPLACE(`problem`, 'Ã¶', 'ö')";
-
-        "UPDATE `groups` SET `name` = REPLACE(`name`, 'Ã–', 'Ö')";
-        "UPDATE `devices` SET `brand` = REPLACE(`brand`, 'Ã–', 'Ö')";
-        "UPDATE `devices` SET `model` = REPLACE(`model`, 'Ã–', 'Ö')";
-        "UPDATE `devices` SET `problem` = REPLACE(`problem`, 'Ã–', 'Ö')";
-
-        "UPDATE `groups` SET `name` = REPLACE(`name`, 'Ã¤', 'ä')";
-        "UPDATE `devices` SET `brand` = REPLACE(`brand`, 'Ã¤', 'ä')";
-        "UPDATE `devices` SET `model` = REPLACE(`model`, 'Ã¤', 'ä')";
-        "UPDATE `devices` SET `problem` = REPLACE(`problem`, 'Ã¤', 'ä')";
-
-        "UPDATE `groups` SET `name` = REPLACE(`name`, 'ÃŸ', 'ß')";
-        "UPDATE `devices` SET `brand` = REPLACE(`brand`, 'ÃŸ', 'ß')";
-        "UPDATE `devices` SET `model` = REPLACE(`model`, 'ÃŸ', 'ß')";
-        "UPDATE `devices` SET `problem` = REPLACE(`problem`, 'ÃŸ', 'ß')";
-
-        "UPDATE `groups` SET `name` = REPLACE(`name`, 'â€™', '\'')";
-        "UPDATE `devices` SET `brand` = REPLACE(`brand`, 'â€™', '\'')";
-        "UPDATE `devices` SET `model` = REPLACE(`model`, 'â€™', '\'')";
-        "UPDATE `devices` SET `problem` = REPLACE(`problem`, 'â€™', '\'')";
-
-        "UPDATE `devices` SET `model` = REPLACE(`model`, 'â€ž', '\"')";
-        "UPDATE `devices` SET `problem` = REPLACE(`problem`, 'â€ž', '\"')";
-
-        "UPDATE `devices` SET `model` = REPLACE(`model`, 'â€œ', '\"')";
-        "UPDATE `devices` SET `problem` = REPLACE(`problem`, 'â€œ', '\"')";
-
-        "UPDATE `groups` SET `name` = REPLACE(`name`, 'Cafe', 'Café')";
-
-        "UPDATE `devices` SET `problem` = REPLACE(`problem`, 'Â£', '£')";
-        "UPDATE `devices` SET `problem` = REPLACE(`problem`, 'â‚¬', '€')";
-        /*
-          SELECT * FROM devices WHERE problem LIKE CONCAT("%", UNHEX('C2A3'), "%");
-
-          SELECT
-          o.`iddevices`
-          ,CASE WHEN o.`brand` <> CONVERT(o.`brand` USING ASCII) THEN o.`brand` END AS in_brand
-          FROM `devices` o
-          WHERE o.`brand` <> CONVERT(o.`brand` USING ASCII)
-          ORDER BY in_brand DESC;
-
-          SELECT
-          o.`iddevices`
-          ,CASE WHEN o.`model` <> CONVERT(o.`model` USING ASCII) THEN o.`model` END AS in_model
-          FROM `devices` o
-          WHERE o.`model` <> CONVERT(o.`model` USING ASCII)
-          ORDER BY in_model DESC;
-
-          SELECT
-          o.`iddevices`
-          ,CASE WHEN o.`problem` <> CONVERT(o.`problem` USING ASCII) THEN o.`problem` END AS in_problem
-          FROM `devices` o
-          WHERE o.`problem` <> CONVERT(o.`problem` USING ASCII)
-          ORDER BY in_problem DESC;
-
-          SELECT
-          o.`idgroups`
-          ,CASE WHEN o.`name` <> CONVERT(o.`name` USING ASCII) THEN o.`name` END AS in_name
-          FROM `groups` o
-          WHERE o.`name` <> CONVERT(o.`name` USING ASCII)
-          ORDER BY in_name DESC;
-
-          SELECT
-          o.`idgroups`
-          ,CASE WHEN o.`location` <> CONVERT(o.`location` USING ASCII) THEN o.`location` END AS in_location
-          FROM `groups` o
-          WHERE o.`location` <> CONVERT(o.`location` USING ASCII)
-          ORDER BY in_location DESC;
-
-          SELECT
-          o.`idgroups`
-          ,CASE WHEN o.`freetext` <> CONVERT(o.`freetext` USING ASCII) THEN o.`freetext` END AS in_freetext
-          FROM `groups` o
-          WHERE o.`freetext` <> CONVERT(o.`freetext` USING ASCII)
-          ORDER BY in_freetext DESC;
-
-          SELECT
-          o.`idevents`
-          ,CASE WHEN o.`location` <> CONVERT(o.`location` USING ASCII) THEN o.`location` END AS in_location
-          FROM `events` o
-          WHERE o.`location` <> CONVERT(o.`location` USING ASCII)
-          ORDER BY in_location DESC;
-
-          SELECT
-          o.`idevents`
-          ,CASE WHEN o.`freetext` <> CONVERT(o.`freetext` USING ASCII) THEN o.`freetext` END AS in_freetext
-          FROM `events` o
-          WHERE o.`freetext` <> CONVERT(o.`freetext` USING ASCII)
-          ORDER BY in_freetext DESC;
-
-          SELECT
-          o.`id`
-          ,CASE WHEN o.`name` <> CONVERT(o.`name` USING ASCII) THEN o.`name` END AS in_name
-          FROM `networks` o
-          WHERE o.`name` <> CONVERT(o.`name` USING ASCII)
-          ORDER BY in_name DESC;
-
-          SELECT
-          o.`id`
-          ,CASE WHEN o.`description` <> CONVERT(o.`description` USING ASCII) THEN o.`description` END AS in_description
-          FROM `networks` o
-          WHERE o.`description` <> CONVERT(o.`description` USING ASCII)
-          ORDER BY in_description DESC;
-
-          SELECT
-          o.`id`
-          ,CASE WHEN o.`name` <> CONVERT(o.`name` USING ASCII) THEN o.`name` END AS in_name
-          FROM `users` o
-          WHERE o.`name` <> CONVERT(o.`name` USING ASCII)
-          ORDER BY in_name DESC;
-
-
-         * */
+    /**
+     * Report char issues.
+     *
+     * @return void
+     */
+    private function _report($prefix = '', $suffix = '') {
+        $tables = $this->_dataTables();
+        $format = 'SELECT
+ o.`%3$s` AS `id`,
+ o.`%2$s` AS `val`
+ FROM `%1$s` o
+ WHERE o.`%2$s` <> CONVERT(o.`%2$s` USING ASCII)
+ ORDER BY `val` DESC';
+        foreach ($tables as $table => $fields) {
+            $key = $fields['key'];
+            foreach ($fields['fields'] as $field) {
+                $qry = sprintf($format, $table, $field, $key);
+                $result = DB::select(DB::raw($qry));
+                $log = [];
+                foreach ($result as $v) {
+                    $log[$v->id] = $v->val;
+                }
+                $filename = $prefix . $table . '_' . $field . $suffix;
+                file_put_contents(storage_path() . "/logs/$filename.log", print_r($log, 1));
+            }
+        }
     }
 
     private function _tableCharsets() {
@@ -246,7 +234,6 @@ class AlterTableCharsets extends Migration {
             ['TABLE_NAME' => 'devices_faults_opinions', 'TABLE_COLLATION' => 'utf8mb4_unicode_ci'],
             ['TABLE_NAME' => 'fault_types_mobiles', 'TABLE_COLLATION' => 'utf8mb4_unicode_ci'],
             ['TABLE_NAME' => 'devices_faults_mobiles_ora_opinions', 'TABLE_COLLATION' => 'utf8mb4_unicode_ci'],
-            ['TABLE_NAME' => 'fault_types', 'TABLE_COLLATION' => 'utf8_general_ci'],
             ['TABLE_NAME' => 'users_groups', 'TABLE_COLLATION' => 'utf8_general_ci'],
             ['TABLE_NAME' => 'events_users', 'TABLE_COLLATION' => 'utf8_general_ci'],
             ['TABLE_NAME' => 'images', 'TABLE_COLLATION' => 'utf8_general_ci'],
@@ -263,8 +250,11 @@ class AlterTableCharsets extends Migration {
             ['TABLE_NAME' => 'links', 'TABLE_COLLATION' => 'utf8_general_ci'],
             ['TABLE_NAME' => 'categories', 'TABLE_COLLATION' => 'utf8_general_ci'],
             ['TABLE_NAME' => 'permissions', 'TABLE_COLLATION' => 'utf8_general_ci'],
-            ['TABLE_NAME' => 'brands_models', 'TABLE_COLLATION' => 'utf8_general_ci'],
-            ['TABLE_NAME' => 'devices_models', 'TABLE_COLLATION' => 'utf8_unicode_ci']
+                /** unused tables / no migration 
+                  ['TABLE_NAME' => 'fault_types', 'TABLE_COLLATION' => 'utf8_general_ci'],
+                  ['TABLE_NAME' => 'brands_models', 'TABLE_COLLATION' => 'utf8_general_ci'],
+                  ['TABLE_NAME' => 'devices_models', 'TABLE_COLLATION' => 'utf8_unicode_ci']
+                 * */
         ];
     }
 
