4316d53557ee792617b793bc9eff535031078a12
Edward Hibbert <edward@ehibbert.org.uk>
2020-11-27 13:23:43 +0000

    Show current branch

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -12,6 +12,22 @@
     </div>
 </a>
 
+@if(env('APP_SHOW_BRANCH'))
+    <div style="position: fixed; top: 0px; left: 0px; color: red">
+        <?php
+        // We want to show the current branch.  This will only be set on development or staging environments.
+        $branch = "Unknown branch";
+        @exec('git branch | ' . "grep ' * '", $shellOutput);
+        foreach ($shellOutput as $line) {
+            if (strpos($line, '* ') !== false) {
+                echo trim(strtolower(str_replace('* ', '', $line)));
+            }
+        }
+        ?>
+        {{ $branch }}
+    </div>
+@endif
+
 {{-- Left side of the Navigation --}}
 <ul class="nav-left">
     <li>
