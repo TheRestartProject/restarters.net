c81fd8c70da588b0775d04a427a9cc8c06dd2b97
Edward Hibbert <edward@ehibbert.org.uk>
2020-12-09 13:24:41 +0000

    Fixometer showing undefined weight for most recent event after search

--- app/Http/Controllers/DeviceController.php
+++ app/Http/Controllers/DeviceController.php
@@ -190,6 +190,9 @@ class DeviceController extends Controller
         ->orderBy('event_date', 'DESC')
         ->first();
 
+        $most_recent_finished_event['id_events'] = $most_recent_finished_event->idevents;
+        $most_recent_finished_event['waste_prevented'] = $most_recent_finished_event->WastePrevented;
+
         $user_groups = Group::with('allRestarters', 'parties', 'groupImage.image')
         ->join('users_groups', 'users_groups.group', '=', 'groups.idgroups')
         ->join('events', 'events.group', '=', 'groups.idgroups')
