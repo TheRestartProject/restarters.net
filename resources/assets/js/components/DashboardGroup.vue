eb3514b8490c248fb6e2187bd23013588df4b065
Edward Hibbert <edward@ehibbert.org.uk>
2021-03-19 11:09:41 +0000

    Follow button on mobile

--- resources/assets/js/components/DashboardGroup.vue
+++ resources/assets/js/components/DashboardGroup.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <div class="d-flex justify-content-between flex-wrap">
+    <div class="d-flex justify-content-between">
       <div class="d-flex">
         <b-img-lazy :src="groupImage" class="profile mr-2" @error.native="brokenProfileImage" />
         <div class="d-flex flex-column justify-content-center">
           <a :href="'/group/view/' + group.idgroups">{{ group.name }}</a>
         </div>
       </div>
-      <b-btn variant="primary" :href="'/group/join/' + group.idgroups" class="mt-2 mt-md-0">
+      <b-btn variant="primary" :href="'/group/join/' + group.idgroups">
         {{ translatedFollowGroup }}
       </b-btn>
     </div>
