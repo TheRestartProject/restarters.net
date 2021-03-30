64f1d382e7742779cb7dbda6e9fb5514081cf149
Zenlan <mszpak@zenlan.com>
2021-03-30 17:24:03 +0100

    Tweaks TabicatOra status SQL - removes redundant joins

--- app/TabicatOra.php
+++ app/TabicatOra.php
@@ -115,14 +115,13 @@ WHERE o.id_ords IS NULL
         $result['total_recats'] = DB::select("
 SELECT COUNT(*) AS total FROM (
 SELECT
-d.id_ords,
+o.id_ords,
 (SELECT o1.fault_type_id FROM devices_faults_tablets_ora_opinions o1 WHERE o1.id_ords = o.id_ords GROUP BY o1.fault_type_id ORDER BY COUNT(o1.fault_type_id) DESC LIMIT 1) AS winning_opinion_id,
 ROUND((SELECT COUNT(o3.fault_type_id) as top_crowd_opinion_count FROM devices_faults_tablets_ora_opinions o3 WHERE o3.id_ords = o.id_ords GROUP BY o3.fault_type_id ORDER BY top_crowd_opinion_count DESC LIMIT 1) /
 (SELECT COUNT(o4.fault_type_id) as all_votes FROM devices_faults_tablets_ora_opinions o4 WHERE o4.id_ords = o.id_ords) * 100) AS top_crowd_opinion_percentage,
 COUNT(o.fault_type_id) AS all_crowd_opinions_count
-FROM `devices_tabicat_ora` d
-LEFT OUTER JOIN devices_faults_tablets_ora_opinions o ON o.id_ords = d.id_ords
-GROUP BY d.id_ords
+FROM devices_faults_tablets_ora_opinions o
+GROUP BY o.id_ords
 HAVING
 (all_crowd_opinions_count > 1 AND top_crowd_opinion_percentage > 60)
 UNION
@@ -138,14 +137,13 @@ FROM devices_faults_tablets_ora_adjudicated a
         $result['list_recats'] = DB::select("
 SELECT result.winning_opinion_id, fta.title as winning_opinion, COUNT(*) AS total FROM (
 SELECT
-d.id_ords,
+o.id_ords,
 (SELECT o1.fault_type_id FROM devices_faults_tablets_ora_opinions o1 WHERE o1.id_ords = o.id_ords GROUP BY o1.fault_type_id ORDER BY COUNT(o1.fault_type_id) DESC LIMIT 1) AS winning_opinion_id,
 ROUND((SELECT COUNT(o3.fault_type_id) as top_crowd_opinion_count FROM devices_faults_tablets_ora_opinions o3 WHERE o3.id_ords = o.id_ords GROUP BY o3.fault_type_id ORDER BY top_crowd_opinion_count DESC LIMIT 1) /
 (SELECT COUNT(o4.fault_type_id) as all_votes FROM devices_faults_tablets_ora_opinions o4 WHERE o4.id_ords = o.id_ords) * 100) AS top_crowd_opinion_percentage,
 COUNT(o.fault_type_id) AS all_crowd_opinions_count
-FROM `devices_tabicat_ora` d
-LEFT OUTER JOIN devices_faults_tablets_ora_opinions o ON o.id_ords = d.id_ords
-GROUP BY d.id_ords
+FROM devices_faults_tablets_ora_opinions o
+GROUP BY o.id_ords
 HAVING
 (all_crowd_opinions_count > 1 AND top_crowd_opinion_percentage > 60)
 UNION
