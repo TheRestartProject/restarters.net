dc6e8972a160562db448bb535f99c1e3001cf3b5
Edward Hibbert <edward@ehibbert.org.uk>
2021-04-07 15:18:37 +0100

    Introduce EventTitle and show when events are online

---
+++ resources/assets/js/components/EventTitle.vue
@@ -0,0 +1,30 @@
+<template>
+  <component :is="component">
+    {{ event.venue ? event.venue : event.location }}
+    <b-badge v-if="event.online" variant="primary" pill>
+      {{ translatedOnline }}
+    </b-badge>
+  </component>
+</template>
+<script>
+export default {
+  props: {
+    idevents: {
+      type: Number,
+      required: true
+    },
+    component: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    event() {
+      return this.$store.getters['events/get'](this.idevents)
+    },
+    translatedOnline() {
+      return this.$lang.get('events.online')
+    }
+  }
+}
+</script>
