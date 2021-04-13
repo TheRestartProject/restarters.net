9d516c0d283a20820256a9fff46f19813d1247c3
Neil Mather <ngmather@gmail.com>
2021-04-13 18:07:39 +0100

    Change copy of info modal

--- resources/views/printcatora/index.blade.php
+++ resources/views/printcatora/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app', ['show_login_join_to_anons' => true, 'hide_language' => false])
+@extends('layouts.app', ['show_navbar_to_anons' => true, 'show_login_join_to_anons' => true, 'hide_language' => false])
 
 @section('extra-css')
 
