e614ac743abec7096d13308f4e60605b35460f05
Neil Mather <ngmather@gmail.com>
2021-04-15 17:29:40 +0100

    Switch to paw prints for icon

--- resources/views/printcatora/index.blade.php
+++ resources/views/printcatora/index.blade.php
@@ -29,41 +29,15 @@
                 <h1 class="pull-left">PrintCat</h1>
             </div>
             <div class="col-6 text-right">
-                <!--
-            These images are licensed under the Creative Commons Attribution 4.0 International license.
-            Attribution: Vincent Le Moign
-            https://commons.wikimedia.org/wiki/Category:SVG_emoji_smilies
-                -->
-                <?php
-                if ($fault) {
-                    switch ($fault->repair_status) {
-                        case 'Fixed': $img = '099-smiling-cat-face-with-heart-eyes-64px.svg.png';
-                            $alt = 'LoveCat';
-                            break;
-                        case 'Repairable': $img = '097-grinning-cat-face-with-smiling-eyes-64px.svg.png';
-                            $alt = 'HappyCat';
-                            break;
-                        case 'End of life': $img = '103-crying-cat-face-64px.svg.png';
-                            $alt = 'SadCat';
-                            break;
-                        case 'Unknown': $img = '102-weary-cat-face-64px.svg.png';
-                            $alt = 'HuhCat';
-                            break;
-                    }
-                } else {
-                    $img = '100-cat-face-with-wry-smile.svg.png';
-                    $alt = 'MehCat';
-                }
-                ?>
                 <a id="btn-info-open"
                    data-toggle="modal" data-target="#printcatoraInfoModal"
-                   class="btn btn-info btn-sm btn-rounded p-2 pull-right">
+                   class="btn btn-info btn-sm btn-rounded p-2 ml-2">
                     <svg style="width:24px;height:24px;" viewBox="0 0 24 24">
                     <title>About PrintCat</title>
                     <path fill="#fff" d="M13.5,4A1.5,1.5 0 0,0 12,5.5A1.5,1.5 0 0,0 13.5,7A1.5,1.5 0 0,0 15,5.5A1.5,1.5 0 0,0 13.5,4M13.14,8.77C11.95,8.87 8.7,11.46 8.7,11.46C8.5,11.61 8.56,11.6 8.72,11.88C8.88,12.15 8.86,12.17 9.05,12.04C9.25,11.91 9.58,11.7 10.13,11.36C12.25,10 10.47,13.14 9.56,18.43C9.2,21.05 11.56,19.7 12.17,19.3C12.77,18.91 14.38,17.8 14.54,17.69C14.76,17.54 14.6,17.42 14.43,17.17C14.31,17 14.19,17.12 14.19,17.12C13.54,17.55 12.35,18.45 12.19,17.88C12,17.31 13.22,13.4 13.89,10.71C14,10.07 14.3,8.67 13.14,8.77Z"></path>
                     </svg></a>
-                <a href="{{ '/printcat/status' . ($partner ? '?partner=$partner' : '') }}" class="pull-right">
-                    <img id="printcat" src="{{ asset('/images/printcatora/'.$img) }}" alt="{{ $alt }}" width="48" height="48" />
+                <a href="{{ '/printcat/status' . ($partner ? '?partner=$partner' : '') }}">
+                    <img id="printcat" src="{{ asset('/images/printcatora/paw-prints.png') }}" alt="paw prints" width="48" height="48" />
                 </a>
             </div>
         </div>
