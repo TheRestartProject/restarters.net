a8dc225d242e4f12b43437fa3a408220b5853880
Neil Mather <ngmather@gmail.com>
2021-04-20 10:20:36 +0100

    Include ORA link in info modal

--- resources/lang/en/printcatora.php
+++ resources/lang/en/printcatora.php
@@ -43,7 +43,7 @@ return [
         'body-s2-header' => 'What do do I need to do?',
         'body-s2-p1' => 'Simply read the information about the broken printer and select the type of fault described from the list underneath. If you’re not sure, just select ‘I don’t know’ at the bottom. Once you’ve selected an option, we’ll show you another printer. The more printer faults you can categorise, the more we learn! PrintCat shows each printer to three people to help confirm the right category.',
         'body-s3-header' => 'Where does PrintCat get data about broken printers?',
-        'body-s3-p1' => 'PrintCat uses information from the Open Repair Alliance, which collects data about broken devices that real people around the world have tried to fix at community events, such as Repair Cafés and Restart Parties.',
+        'body-s3-p1' => 'PrintCat uses information from the <a href=":url" target="_blank">Open Repair Alliance</a>, which collects data about broken devices that real people around the world have tried to fix at community events, such as Repair Cafés and Restart Parties.',
         'body-s4-header' => 'More questions?',
         'body-s4-p1' => 'Find out more, and give us your feedback, in the <a href=":url" target="_blank">PrintCat discussion</a>.',
     ],
