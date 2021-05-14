21da94788cb589661d082c249dea8c3ae677eca6
Edward Hibbert <edward@ehibbert.org.uk>
2021-05-14 15:41:54 +0100

    Test fixometer page loads

---
+++ tests/Feature/Fixometer/BasicTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature\Fixometer;
+
+use DB;
+use Hash;
+use Mockery;
+use Tests\TestCase;
+
+class BasicTest extends TestCase {
+    public function testDashboardPageLoads() {
+        // Test the dashboard page loads.  Most of the work is done inside Vue, so a basic test is just that the
+        // Vue component exists.
+        $this->loginAsTestUser();
+        $response = $this->get('/fixometer');
+        $content = $response->getContent();
+
+        $this->assertNotFalse(strpos($content, '<FixometerPage'));
+    }
+}
