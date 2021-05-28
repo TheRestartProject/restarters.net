6b081a09cf2afffa549167fe565d3864c726d5de
Edward Hibbert <edward@ehibbert.org.uk>
2021-05-28 10:49:25 +0100

    Don't count cancelled events in stats

--- app/Party.php
+++ app/Party.php
@@ -39,7 +39,7 @@ class Party extends Model implements Auditable
         'shareable_code',
         'online',
     ];
-    protected $hidden = ['created_at', 'updated_at', 'deleted_at', 'frequency', 'group', 'group', 'user_id', 'wordpress_post_id'];
+    protected $hidden = ['created_at', 'updated_at', 'deleted_at', 'frequency', 'group', 'group', 'user_id', 'wordpress_post_id', 'cancelled'];
 
     // Append data to Model
     protected $appends = ['participants', 'ShareableLink'];
@@ -705,16 +705,21 @@ class Party extends Model implements Auditable
 
     public function hoursVolunteered()
     {
-        $lengthOfEventInHours = 3;
-        $extraHostHours = 9;
-        $hoursIfNoVolunteersRecorded = 12;
+        if (!$this->cancelled) {
+            $lengthOfEventInHours = 3;
+            $extraHostHours = 9;
+            $hoursIfNoVolunteersRecorded = 12;
 
-        $hoursVolunteered = $extraHostHours;
+            $hoursVolunteered = $extraHostHours;
 
-        if ($this->volunteers > 0) {
-            $hoursVolunteered += $this->volunteers * $lengthOfEventInHours;
+            if ($this->volunteers > 0) {
+                $hoursVolunteered += $this->volunteers * $lengthOfEventInHours;
+            } else {
+                $hoursVolunteered += $hoursIfNoVolunteersRecorded;
+            }
         } else {
-            $hoursVolunteered += $hoursIfNoVolunteersRecorded;
+            // Cancelled events are assumed to take 3 hours.
+            $hoursVolunteered = 3;
         }
 
         return $hoursVolunteered;
