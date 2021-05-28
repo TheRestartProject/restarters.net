da067f75504f6f372f29c4f30a1d9b6b332e2ae5
zenlan <monique.szpak@therestartproject.org>
2021-05-28 07:17:23 +0100

    Tidies expressions in TabiCat status blade

--- resources/views/tabicatora/status.blade.php
+++ resources/views/tabicatora/status.blade.php
@@ -23,16 +23,14 @@
 
 <section class="tabicat">
     <div class="container mt-1 mt-sm-4">
-    <div class="row row-compressed align-items-center">
+        <div class="row row-compressed align-items-center">
             <div class="col-5">
                 <h1 class="pull-left">TabiCat @lang('tabicatora.status.status')
                     <img id="tabicat" class="pull-left d-none d-lg-block" src="{{ asset('/images/tabicatora/paw-prints.png') }}" alt="TabiCat status" />
                 </h1>
             </div>
             <div class="col-7 text-right">
-                <a id="btn-info-open"
-                   data-toggle="modal" data-target="#tabicatoraInfoModal"
-                   class="btn btn-primary ml-2">
+                <a id="btn-info-open" data-toggle="modal" data-target="#tabicatoraInfoModal" class="btn btn-primary ml-2">
                     @lang('tabicatora.about')
                 </a>
                 <a class="btn btn-primary" href="{{ '/tabicat' }}">
@@ -56,31 +54,31 @@
                     <div class="col">
                         <p class="badge-pill badge-light"><span>@lang('tabicatora.status.total')</span></p>
                         <p>
-                            @php( print($status['total_devices'][0]->total))
+                            {{ $status['total_devices'][0]->total }}
                         </p>
                     </div>
                     <div class="col">
                         <p class="badge-pill badge-light"><span>@lang('tabicatora.status.items_3_opinions')</span></p>
                         <p>
-                            @php( print($status['total_opinions_3'][0]->total))
+                            {{ $status['total_opinions_3'][0]->total }}
                         </p>
                     </div>
                     <div class="col">
                         <p class="badge-pill badge-light"><span>@lang('tabicatora.status.items_2_opinions')</span></p>
                         <p>
-                            @php( print($status['total_opinions_2'][0]->total))
+                            {{ $status['total_opinions_2'][0]->total }}
                         </p>
                     </div>
                     <div class="col">
                         <p class="badge-pill badge-light"><span>@lang('tabicatora.status.items_1_opinion')</span></p>
                         <p>
-                            @php( print($status['total_opinions_1'][0]->total))
+                            {{ $status['total_opinions_1'][0]->total }}
                         </p>
                     </div>
                     <div class="col">
                         <p class="badge-pill badge-light"><span>@lang('tabicatora.status.items_0_opinions')</span></p>
                         <p>
-                            @php( print($status['total_opinions_0'][0]->total))
+                            {{ $status['total_opinions_0'][0]->total }}
                         </p>
                     </div>
                 </div>
@@ -89,15 +87,15 @@
         <div class="row problem panel p-2 mb-4 mx-1 mx-sm-0 justify-content-center">
             <div class="col">
                 <div class="row justify-content-center">
-                    <p><strong>@php( print($status['progress'][0]->total))% @lang('tabicatora.status.progress')</strong></p>
+                    <p><strong>{{ $status['progress'][0]->total }}% @lang('tabicatora.status.progress')</strong></p>
                 </div>
-             </div>
+            </div>
         </div>
         @endif
         <div class="row problem panel p-2 mb-4 mx-1 mx-sm-0 justify-content-center">
             <div class="col">
                 <div class="row justify-content-center">
-                    <p><strong>@lang('tabicatora.status.items_majority_opinions') : @php( print($status['total_recats'][0]->total)) </strong></p>
+                    <p><strong>@lang('tabicatora.status.items_majority_opinions') : {{ $status['total_recats'][0]->total }} </strong></p>
                 </div>
                 <div class="row justify-content-center">
                     <div class="col">
@@ -116,7 +114,7 @@
                         @foreach($status['list_recats'] as $row)
                         <div class="row border-grey">
                             <div class="col col-2">
-                                @php( print($row->total) )
+                                {{ $row->total }}
                             </div>
                             <div class="col">
                                 @lang($row->winning_opinion)
@@ -132,7 +130,7 @@
         <div class="row problem panel p-2 mb-4 mx-1 mx-sm-0 justify-content-center">
             <div class="col">
                 <div class="row justify-content-center">
-                    <p><strong>@lang('tabicatora.status.items_split_opinions') : @php( print($status['total_splits'][0]->total))</strong></p>
+                    <p><strong>@lang('tabicatora.status.items_split_opinions') : {{ $status['total_splits'][0]->total }}</strong></p>
                 </div>
                 <div class="row justify-content-center">
                     <div class="col">
@@ -157,16 +155,20 @@
                         @foreach($status['list_splits'] as $row)
                         <div class="row border-grey">
                             <div class="col col-1">
-                                @php( print($row->id_ords) )
+                                {{ $row->id_ords }}
+                                <!-- @php( print($row->id_ords) ) -->
                             </div>
                             <div class="col col-3">
-                                @php( print($row->opinions) )
+                                {{ $row->opinions }}
+                                <!-- @php( print($row->opinions) ) -->
                             </div>
                             <div class="col col-2">
-                                @php( print($row->brand) )
+                                {{ $row->brand }}
+                                <!-- @php( print($row->brand) ) -->
                             </div>
                             <div class="col">
-                                @php( print($row->problem) )
+                                {{ $row->problem }}
+                                <!-- @php( print($row->problem) ) -->
                             </div>
                         </div>
                         @endforeach
