a1c94a1e2a3ee461d11c23c12da5b189ed9e2f34
Edward Hibbert <edward@ehibbert.org.uk>
2021-06-04 09:10:32 +0100

    WIP: Change events table to allow filtering

--- resources/assets/js/components/GroupEventScrollTable.vue
+++ resources/assets/js/components/GroupEventScrollTable.vue
@@ -27,15 +27,20 @@
           date: true,
           'text-left': true,
           'pl-3': true,
-          attending: data.item.date_short.attending
+          'w-100': true,
+          attending: data.item.date_long.attending
         }">
           {{ date(data.item.date_short) }}
-          <br class="d-none d-md-block" />
+          <br class="hidecell" />
           {{ start(data.item.date_short) }} <span class="d-none d-md-inline">- {{ end(data.item.date_short) }}</span>
           <br class="d-block d-md-none" />
-          <!--          TODO Event title?-->
         </div>
-        <div class="text-left d-none d-md-table-cell pl-3">
+        <div :class="{
+          'text-left': true,
+          'hidecell': true,
+          'pl-3': true,
+           attending: data.item.date_long.attending
+        }">
           <span v-if="addGroupName" class="small">
             <a :href="'/group/view/' + data.item.date_short.group.idgroups">{{ data.item.date_short.group.name }}</a>
           </span>
@@ -47,7 +52,7 @@
       </template>
       <template slot="cell(title)" slot-scope="data" v-bind:attending="attending">
         <a :class="{
-          attending: data.item.date_short.attending
+          attending: data.item.title.attending
         }">
           <!-- eslint-disable-next-line -->
           <b>{{ data.item.title.venue ? data.item.title.venue : data.item.title.location }}<b-badge v-if="data.item.title.online" variant="primary" pill class="nounderline">{{ __('events.online') }}</b-badge></b>
@@ -55,44 +60,189 @@
       </template>
 
       <template slot="head(invited)">
-        <div class="d-none d-md-table-cell">
+        <div class="hidecell">
           <b-img class="icon" src="/images/mail_ico.svg" :title="__('groups.volunteers_invited')" />
         </div>
       </template>
       <template slot="cell(invited)" slot-scope="data" v-bind:attending="attending">
         <div :class="{
-          'd-none': true,
-          'd-md-table-cell': true,
+          'hidecell': true,
           'cell-number': true,
-          attending: data.item.date_short.attending
+          attending: data.item.invited.attending
         }">
-          {{ data.item.invited }}
+          {{ data.item.invited.invited || '0' }}
         </div>
       </template>
 
       <template slot="head(volunteers)">
-        <div class="d-none d-md-table-cell">
+        <div class="hidecell">
           <b-img class="icon" src="/images/participants.svg" :title="__('groups.volunteers_confirmed')" />
         </div>
       </template>
       <template slot="cell(volunteers)" slot-scope="data" v-bind:attending="attending">
         <div :class="{
-          'd-none': true,
-          'd-md-table-cell': true,
+          'hidecell': true,
           'cell-number': true,
-          attending: data.item.date_short.attending
+          attending: data.item.volunteers.attending
         }">
-          {{ data.item.volunteers }}
+          {{ data.item.volunteers.volunteers || '0' }}
         </div>
       </template>
 
-      <template slot="cell(actions)" slot-scope="data" v-bind:attending="attending">
+      <template slot="head(actions)">
+        <span />
+      </template>
+      <template slot="cell(actions)" slot-scope="data" v-bind:attending="attending" v-bind:upcoming="upcoming">
         <div :class="{
-          attending: data.item.date_short.attending
+          attending: data.item.actions.attending
         }">
-          TODO
+          <div v-if="upcoming(data.item.actions)">
+            <div v-if="data.item.actions.requiresModeration" class="cell-warning">
+              <span v-if="data.item.actions.canModerate">
+                <a :href="'/party/edit/' + data.item.actions.idevents">{{ __('partials.event_requires_moderation') }}</a>
+              </span>
+              <span v-else>
+                {{ __('partials.event_requires_moderation_by_an_admin') }}
+              </span>
+            </div>
+            <div v-else class="hidecell">
+              <div v-if="data.item.actions.attending" class="text-black font-weight-bold d-flex justify-content-around">
+                <span>
+                  {{ __('events.youre_going') }}
+                </span>
+              </div>
+              <!-- "all" or "nearby" events are for ones where we're not a member, so show a join button. -->
+              <b-btn variant="primary" :href="'/group/join/' + data.item.actions.group.idgroups" v-else-if="data.item.actions.all || data.item.actions.nearby">
+                {{ __('groups.join_group_button') }}
+              </b-btn>
+              <!-- We can't RSVP if the event is starting soon. -->
+              <b-btn variant="primary" :href="'/party/join/' + data.item.actions.idevents" :disabled="startingSoon(data.item.actions)" v-else>
+                {{ __('events.RSVP') }}
+              </b-btn>
+            </div>
+          </div>
         </div>
       </template>
+
+      <template slot="head(participants_count)">
+        <div class="hidecell">
+          <b-img class="icon" src="/images/participants.svg" :title="__('groups.participants_attended')" />
+        </div>
+      </template>
+      <template slot="cell(participants_count)" slot-scope="data" v-bind:attending="attending">
+        <div :class="{
+          'hidecell': true,
+          'cell-number': true,
+          'cell-danger': data.item.participants_count.participants_count === 0,
+          attending: data.item.participants_count.attending
+        }">
+          {{ data.item.participants_count.participants_count || '0' }}
+        </div>
+      </template>
+
+      <template slot="head(volunteers_count)">
+        <div class="hidecell">
+          <b-img class="icon" src="/icons/volunteer_ico-thick.svg" :title="__('groups.volunteers_attended')" />
+        </div>
+      </template>
+      <template slot="cell(volunteers_count)" slot-scope="data" v-bind:attending="attending">
+        <div :class="{
+          'hidecell': true,
+          'cell-number': true,
+          'cell-danger': data.item.volunteers_count.volunteers_count === 0,
+          attending: data.item.volunteers_count.attending
+        }">
+          {{ data.item.volunteers_count.volunteers_count || '0' }}
+        </div>
+      </template>
+
+      <template slot="head(ewaste)">
+        <div class="hidecell">
+          <b-img class="icon" src="/images/trash.svg" :title="__('groups.waste_prevented')" />
+        </div>
+      </template>
+      <template slot="cell(ewaste)" slot-scope="data" v-bind:attending="attending" v-bind="stats">
+        <div :class="{
+          'hidecell': true,
+          'cell-number': true,
+          attending: data.item.ewaste.attending
+        }"
+         v-if="stats(data.item.ewaste)"
+        >
+          {{ Math.round(stats(data.item.ewaste).ewaste) }} kg
+        </div>
+      </template>
+
+      <template slot="head(co2)">
+        <div class="hidecell">
+          <b-img class="icon" src="/images/cloud_empty.svg" :title="__('groups.co2_emissions_prevented')" />
+        </div>
+      </template>
+      <template slot="cell(co2)" slot-scope="data" v-bind:attending="attending" v-bind="stats">
+        <div :class="{
+          'hidecell': true,
+          'cell-number': true,
+          attending: data.item.co2.attending
+        }"
+         v-if="stats(data.item.co2)"
+        >
+          {{ Math.round(stats(data.item.co2)).co2 }} kg
+        </div>
+      </template>
+
+      <template slot="head(fixed_devices)">
+        <div class="hidecell">
+          <b-img class="icon" src="/images/fixed.svg" :title="__('groups.fixed_items')" />
+        </div>
+      </template>
+      <template slot="cell(fixed_devices)" slot-scope="data" v-bind:attending="attending" v-bind="stats">
+        <div :class="{
+          'hidecell': true,
+          'cell-number': true,
+          attending: data.item.fixed_devices.attending
+        }"
+         v-if="stats(data.item.fixed_devices)"
+        >
+          {{ stats(data.item.fixed_devices).fixed_devices || '0'}}
+        </div>
+      </template>
+
+      <template slot="head(repairable_devices)">
+        <div class="hidecell">
+          <b-img class="icon" src="/images/repairable_ico.svg" :title="__('groups.repairable_items')" />
+        </div>
+      </template>
+      <template slot="cell(repairable_devices)" slot-scope="data" v-bind:attending="attending" v-bind="stats">
+        <div :class="{
+          'hidecell': true,
+          'cell-number': true,
+          attending: data.item.repairable_devices.attending
+        }"
+         v-if="stats(data.item.repairable_devices)"
+        >
+          {{ stats(data.item.repairable_devices).repairable_devices || '0' }}
+        </div>
+      </template>
+
+      <template slot="head(dead_devices)">
+        <div class="hidecell">
+          <b-img class="icon" src="/images/dead_ico.svg" :title="__('groups.end_of_life_items')" />
+        </div>
+      </template>
+      <template slot="cell(dead_devices)" slot-scope="data" v-bind:attending="attending" v-bind="stats">
+        <div :class="{
+          'hidecell': true,
+          'cell-number': true,
+          attending: data.item.dead_devices.attending
+        }"
+           v-if="stats(data.item.dead_devices)"
+        >
+          {{ stats(data.item.dead_devices).dead_devices || '0' }}
+        </div>
+      </template>
+
+      <!--      TODO No devices warning -->
+
     </b-table>
   </div>
 </template>
@@ -150,6 +300,13 @@ export default {
           { key: 'date_long', label: 'Long Date', sortable: true },
           { key: 'title', label: 'Event Title', sortable: true },
           { key: 'actions', label: 'Actions', },
+          { key: 'participants_count', label: 'Participants', sortable: true},
+          { key: 'volunteers_count', label: 'Volunteers', sortable: true},
+          { key: 'ewaste', label: 'ewaste', sortable: true},
+          { key: 'co2', label: 'co2', sortable: true},
+          { key: 'fixed_devices', label: 'Fixed Devices', sortable: true},
+          { key: 'repairable_devices', label: 'Repairable Devices', sortable: true},
+          { key: 'dead_devices', label: 'Dead Devices', sortable: true},
         ]
       } else {
         return [
@@ -169,9 +326,14 @@ export default {
             date_short: e,
             date_long: e,
             title: e,
-            invited: e.allinvitedcount || '0',
-            volunteers: e.volunteers || '0',
-            actions: e
+            actions: e,
+            participants_count: e,
+            volunteers_count: e,
+            ewaste: e,
+            co2: e,
+            fixed_devices: e,
+            repairable_devices: e,
+            dead_devices: e,
           }
         })
       } else {
@@ -180,13 +342,22 @@ export default {
             date_short: e,
             date_long: e,
             title: e,
-            invited: e.allinvitedcount,
-            volunteers: e.volunteers,
+            invited: e,
+            volunteers: e,
             actions: e
           }
         })
       }
     },
+  },
+  mounted() {
+    // We have some classes which we want to apply to the table cells to colour the background.  The use of slots
+    // and the way b-table works precludes us from applying them to the TD directly.  There are CSS attempts to do this
+    // which don't work very well, and CSS has:() isn't supported yet.  So we have to hack this via JS - look for
+    // the relevant classes and apply them to the parent too.
+    this.$nextTick(() => {
+      // this.$el.querySelector()
+    })
   },
   methods: {
     sortCompare(aRow, bRow, key, sortDesc, formatter, compareOptions, compareLocale) {
@@ -242,6 +413,10 @@ export default {
     },
     volunteerCount(event) {
       return event && event.volunteers ? event.volunteers : 0
+    },
+    stats(event) {
+      console.log("Stats", event, this.$store.getters['events/getStats'](event.idevents))
+      return this.$store.getters['events/getStats'](event.idevents)
     },
   }
 }
@@ -306,11 +481,15 @@ export default {
   display: none;
 
   @include media-breakpoint-up(md) {
-    display: table-cell;
+    display: block;
   }
 }
 
 /deep/ .nounderline {
   text-decoration: none !important;
+}
+
+/deep/ .icon {
+  width: 30px;
 }
 </style>
