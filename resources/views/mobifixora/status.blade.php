37568c7e9032da39774df07d9969216c1e98e363
zenlan <monique.szpak@therestartproject.org>
2021-06-21 16:28:58 +0100

    Adds closed message to MobiFix:ORA status page

--- resources/views/mobifixora/status.blade.php
+++ resources/views/mobifixora/status.blade.php
@@ -41,16 +41,25 @@
                     <path fill="#fff" d="M13.5,4A1.5,1.5 0 0,0 12,5.5A1.5,1.5 0 0,0 13.5,7A1.5,1.5 0 0,0 15,5.5A1.5,1.5 0 0,0 13.5,4M13.14,8.77C11.95,8.87 8.7,11.46 8.7,11.46C8.5,11.61 8.56,11.6 8.72,11.88C8.88,12.15 8.86,12.17 9.05,12.04C9.25,11.91 9.58,11.7 10.13,11.36C12.25,10 10.47,13.14 9.56,18.43C9.2,21.05 11.56,19.7 12.17,19.3C12.77,18.91 14.38,17.8 14.54,17.69C14.76,17.54 14.6,17.42 14.43,17.17C14.31,17 14.19,17.12 14.19,17.12C13.54,17.55 12.35,18.45 12.19,17.88C12,17.31 13.22,13.4 13.89,10.71C14,10.07 14.3,8.67 13.14,8.77Z"></path>
                     </svg></a>
                 @php( $back = '/mobifixora' . ($partner ? "?partner=$partner" : '') )
-                <a href="{{ $back }}">
+                <!-- <a href="{{ $back }}">
                     <img id="mobifix" src="{{ asset('/images/mobifix/whale-spouting.png') }}" alt="Go to MobiFix:ORA" width="48" height="48" />
-                </a>
+                </a> -->
             </div>
         </div>
-        @if(session()->has('success'))
-        <div class="row problem panel p-2 mb-4 mx-1 mx-sm-0 justify-content-center">
-            You've seen them all, thanks!
+        <div class="row problem panel p-2 mb-4 mx-1 mx-sm-0">
+            <div class="col text-left">
+                <h4 class="">This quest is complete 🎉</h4>
+                <p>
+                    Thank you for your interest in this quest.
+                    We have now received enough responses.
+                    Instead, why not see what we learned or try another quest?
+                </p>
+                <ul>
+                    <li><a href="https://talk.restarters.net/t/help-us-understand-why-smartphones-break-with-mobifix-ora-edition/4553">Discover what we learned</a></li>
+                    <li><a href="https://restarters.net/workbench">See our other quests</a></li>
+                </ul>
+            </div>
         </div>
-        @endif
         @if (isset($status))
         @if (!$complete)
         <div class="row problem panel p-2 mb-4 mx-1 mx-sm-0 justify-content-center">
