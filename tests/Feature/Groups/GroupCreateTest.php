8d8e90ae4fdea84f441061c30cdbaa5021c74ade
Edward Hibbert <edward@ehibbert.org.uk>
2021-06-23 17:40:59 +0100

    Handle duplicate group name in creation

--- tests/Feature/Groups/GroupCreateTest.php
+++ tests/Feature/Groups/GroupCreateTest.php
@@ -10,4 +10,20 @@ class GroupCreateTest extends TestCase {
         $this->loginAsTestUser(Role::ADMINISTRATOR);
         $this->assertNotNull($this->createGroup());
     }
+
+    public function testDuplicate() {
+        $this->loginAsTestUser(Role::ADMINISTRATOR);
+
+        // Test creating the same group twice.
+        $this->assertNotNull($this->createGroup());
+
+        $response = $this->post('/group/create',  [
+            'name' => "Test Group0",
+            'website' => 'https://therestartproject.org',
+            'location' => 'London',
+            'free_text' => 'Some text.'
+        ]);
+
+        $this->assertContains('That group name already exists', $response->getContent());
+    }
 }
