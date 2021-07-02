71c5f9b3837374d53254711fbad8895fc65be82c
Edward Hibbert <edward@ehibbert.org.uk>
2021-07-02 17:32:28 +0100

    Test user menus

---
+++ tests/Feature/Users/MenusTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Events\UserUpdated;
+use App\User;
+
+use DB;
+use Carbon\Carbon;
+use Tests\TestCase;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class MenusTest extends TestCase
+{
+    public function provider() {
+        return [
+            [
+                'Administrator',
+                [
+                    0 => 'Administrator',
+                    1 => 'Reporting',
+                    2 => 'General'
+                ]
+            ],
+            [
+                'NetworkCoordinator',
+                [
+                    0 => 'Administrator',
+                    1 => 'General'
+                ]
+            ],
+            [
+                'Host',
+                [
+                    0 => 'Reporting',
+                    1 => 'General'
+                ]
+            ],
+            [
+                'Restarter',
+                [
+                    0 => 'Reporting',
+                    1 => 'General'
+                ]
+            ]
+        ];
+    }
+
+    /**
+     *@dataProvider provider
+     */
+    public function testSections($role, $present)
+    {
+        $user = factory(User::class)->states($role)->create();
+        $this->actingAs($user);
+
+        $response = $this->get('/user/menus');
+        $menus = json_decode($response->getContent(), TRUE);
+
+        $this->assertEquals($present, array_keys($menus));
+    }
+}
