e1eb38130d25879ebb9dd761764e1743e7c805fd
Edward Hibbert <edward@ehibbert.org.uk>
2021-08-25 11:46:14 +0100

    WIP Try coverage in playwright.

--- tests/Integration/landingpage.test.js
+++ tests/Integration/landingpage.test.js
@@ -1,8 +1,43 @@
-const { test, expect } = require('@playwright/test');
+const {test, expect} = require('@playwright/test')
+const {chromium} = require('playwright')
+const v8toIstanbul = require('v8-to-istanbul')
+const fs = require('fs')
+const path = require('path')
+const crypto = require("crypto");
+const COVERAGE_DIRECTORY = '.nyc_output'
 
-test('Landing page has sign in', async ({ page, baseURL }) => {
+// We were hoping to be able to merge these using nyc report but it fails - ran out of time.  If we could,
+// then we could upload to coveralls.
+
+// test.beforeEach(async ({page}) => {
+//   // Not available in all browsers.
+//   if (page.coverage) {
+//     await page.coverage.startJSCoverage()
+//   }
+// })
+//
+// test.afterEach(async ({page}) => {
+//   if (page.coverage) {
+//     if (!fs.existsSync(COVERAGE_DIRECTORY)) {
+//       await fs.promises.mkdir(COVERAGE_DIRECTORY)
+//     }
+//
+//     const coverage = await page.coverage.stopJSCoverage()
+//     for (const entry of coverage) {
+//       const converter = new v8toIstanbul('', 0, {source: entry.source})
+//       await converter.load()
+//       converter.applyCoverage(entry.functions)
+//       const coverageJSON = JSON.stringify(converter.toIstanbul())
+//       const uid = crypto.randomBytes(16).toString("hex");
+//
+//       fs.writeFileSync(path.join(COVERAGE_DIRECTORY, 'playwright_coverage_' + uid + '.json'), coverageJSON)
+//     }
+//   }
+// })
+
+test('Landing page has sign in', async ({page, baseURL}) => {
   // Simple test of page which is rendered with a Laravel blade.
-  await page.goto(baseURL);
+  await page.goto(baseURL)
   const legend = page.locator('legend')
-  await expect(legend).toHaveText('Sign in');
-});
+  await expect(legend).toHaveText('Sign in')
+})
