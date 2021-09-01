34d2fc9a2aa58f235888bc57c3042c152afa5af4
edwh <edward@therestartproject.org>
2021-09-01 11:21:51 +0000

    Merge problem

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -90,28 +90,6 @@
           }
       }
 
-      $expanded_events = [];
-
-      foreach (array_merge($upcoming_events->all(), $past_events->all()) as $event) {
-          $expanded_events[] = \App\Http\Controllers\PartyController::expandEvent($event, $group, $emissionRatio);
-      }
-
-      if ($upcoming_events_in_area) {
-          foreach ($upcoming_events_in_area as $event) {
-            $e = \App\Http\Controllers\PartyController::expandEvent($event, $group, $emissionRatio);
-              $e['nearby'] = TRUE;
-              $expanded_events[] = $e;
-          }
-      }
-
-      if ($upcoming_events_all) {
-          foreach ($upcoming_events_all as $event) {
-            $e = \App\Http\Controllers\PartyController::expandEvent($event, $group, $emissionRatio);
-              $e['all'] = TRUE;
-              $expanded_events[] = $e;
-          }
-      }
-
       ?>
 
     <div class="vue-placeholder vue-placeholder-large">
