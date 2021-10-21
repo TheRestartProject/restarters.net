1327be2ced732f9b35ddff5508db37ba6feb5a5d
Neil Mather <ngmather@gmail.com>
2021-10-21 16:39:36 +0100

    Include APP_ENV and DB_DATABASE when listing instances

--- tools/list-restarters-instances.sh
+++ tools/list-restarters-instances.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
+YELLOW='\033[1;33m'
+CYAN='\033[1;36m'
+RESET='\033[0m'
+
+print_info () {
+        echo -e "\t${CYAN}$1${RESET}:         \t$2"
+}
+
 for instance in restarters.dev*; do
 	cd $instance
-	echo $instance `git status | grep 'On branch'`
+	BRANCH=`git status | grep 'On branch' | sed 's/On branch //'`
+	APP_ENV=`grep APP_ENV .env | sed 's/APP_ENV=//'`
+	DB=`grep DB_DATABASE .env | sed 's/DB_DATABASE=//'`
+        echo -e "${YELLOW}$instance${RESET}"
+	print_info "Branch" $BRANCH
+	print_info "APP_ENV" $APP_ENV
+	print_info "DB_DATABASE" $DB
+	echo
 	cd ..
 done
