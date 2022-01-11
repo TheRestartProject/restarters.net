1c67bd8e906548f88bb45774353b1f10e79d1e10
Edward Hibbert <edward@ehibbert.org.uk>
2022-01-11 17:08:24 +0000

    UT fixes.

--- tests/Integration/landingpage.test.js
+++ tests/Integration/landingpage.test.js
@@ -1,11 +1,11 @@
 const {test, expect} = require('@playwright/test')
 const { login } = require('./utils')
 
-test('Landing page has sign in', async ({page, baseURL}) => {
+test('Landing page has blurb', async ({page, baseURL}) => {
   // Simple test of page which is rendered with a Laravel blade.
   await page.goto(baseURL)
-  const legend = page.locator('legend')
-  await expect(legend).toHaveText('Sign in')
+  const legend = page.locator('h2').first()
+  await expect(legend).toHaveText('Learn and share repair skills with others')
 })
 
 test('Can log in', async({page, baseURL}) => {
