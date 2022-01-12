f9e2307ac0f45dcdaee7e9430a4c8c6165e05e0d
Edward Hibbert <edward@ehibbert.org.uk>
2022-01-12 10:00:35 +0000

    Add meta tags.

--- resources/views/layouts/header_plain.blade.php
+++ resources/views/layouts/header_plain.blade.php
@@ -29,7 +29,7 @@
         <!-- Meta tags for social previews. -->
         <meta data-hid="og:type" property="og:type" content="website">
         <meta data-hid="description" name="description" content="{{ __('landing.intro1') . ' ' . __('landing.intro2') }}">
-        <meta data-hid="og:image" property="og:image" content="{{ url('/images/logo.png') }}">
+        <meta data-hid="og:image" property="og:image" content="{{ url('/images/landing/landing1.jpg') }}">
         <meta data-hid="og:locale" property="og:locale" content="en_GB">
         <meta data-hid="og:title" property="og:title" content="{{ config('app.name', 'Laravel') }}">
         <meta data-hid="og:site_name" property="og:site_name" content="{{ config('app.name', 'Laravel') }}">
@@ -37,7 +37,7 @@
         <meta data-hid="og:description" property="og:description" content="{{ __('landing.intro1') . ' ' . __('landing.intro2') }}">
         <meta data-hid="twitter:title" name="twitter:title" content="{{ config('app.name', 'Laravel') }}">
         <meta data-hid="twitter:description" name="twitter:description" content="{{ __('landing.intro1') . ' ' . __('landing.intro2') }}">
-        <meta data-hid="twitter:image" name="twitter:image" content="{{ url('/images/logo.png') }}">
+        <meta data-hid="twitter:image" name="twitter:image" content="{{ url('/images/landing/landing1.jpg') }}">
         <meta data-hid="twitter:image:alt" name="twitter:image:alt" content="The Restart logo">
         <meta data-hid="twitter:card" name="twitter:card" content="summary_large_image">
         <meta data-hid="twitter:site" name="twitter:site" content="RestartProject">
