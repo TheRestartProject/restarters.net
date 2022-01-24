e2329c5acf79fed5cc1fabcf5f2f0b3c803d80d8
Edward Hibbert <edward@ehibbert.org.uk>
2022-01-24 09:58:13 +0000

    Remove /post-edit code which is not exposed any more.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -403,38 +403,6 @@ class UserController extends Controller
         return redirect()->back()->with('message', 'Admin settings updated');
     }
 
-    public function postEdit(Request $request)
-    {
-        $user = User::find($request->input('id'));
-
-        $check_password = Hash::check($request->input('password'), $user->password);
-
-        if (! is_null($request->input('new-password')) && ! $check_password) {
-            return redirect()
-            ->back()
-            ->withErrors('Incorrect old password - please try again');
-        }
-
-        Validator::make($request->all(), [
-        'name' => 'required|max:255',
-        'email' => 'required|unique:users,email,'.$user->id.'|max:255',
-        'location' => 'max:191',
-        'new-password' => 'confirmed',
-        ])->validate();
-
-        $user->name = $request->input('name');
-        $user->email = $request->input('email');
-        $user->age = $request->input('age');
-        $user->gender = $request->input('gender');
-        $user->location = $request->input('location');
-        if (! empty($request->input('new-password'))) {
-            $user->setPassword(Hash::make($request->input('new-password')));
-        }
-        $user->save();
-
-        return redirect()->back()->with('success', 'Profile updated');
-    }
-
     public function recover(Request $request)
     {
         $User = new User;
