d9e8e2416383a439704ac82cd904d99af278912f
edwh <edward@therestartproject.org>
2022-02-16 09:51:52 +0000

    Build assets

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -154,7 +154,7 @@ final class Versions
   'vlucas/phpdotenv' => 'v3.6.9@a1bf4c9853d90ade427b4efe35355fc41b3d6988',
   'wouternl/laravel-drip' => '1.2.4@f9d96140ba62f4ddb4df909e20931e897e0edd54',
   'yandex/translate-api' => '1.5.2@c99e69cde3e688fc0f99c4d8a21585226a8e1938',
-  'laravel/laravel' => 'dev-production@331694676f734d6f77f2a55a6d4ff40c847e84d5',
+  'laravel/laravel' => 'dev-production@1877109ca7eab6e11dc135cda0840616fa2eb943',
 );
 
     private function __construct()
