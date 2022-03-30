5c72205cf1e87c7a88b6367e3968da517a42c0e6
Edward Hibbert <edward@ehibbert.org.uk>
2022-03-30 15:18:08 +0100

    Add tests of cookie policy and category page.

--- tests/Feature/Dashboard/CookiePolicyTest.php
+++ tests/Feature/Dashboard/CookiePolicyTest.php
@@ -2,16 +2,9 @@
 
 namespace Tests\Feature\Dashboard;
 
-use App\Group;
-use App\Party;
-use App\Role;
-use App\User;
 use DB;
 use Hash;
-use Mockery;
 use Tests\TestCase;
-
-use function PHPUnit\Framework\assertEquals;
 
 class CookiePolicyTest extends TestCase
 {
