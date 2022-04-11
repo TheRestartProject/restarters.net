09dbf00edeaa9d8f5724f3537ff0356552f36e8d
Edward Hibbert <edward@ehibbert.org.uk>
2022-04-11 16:01:46 +0100

    Test fixes.

--- app/Listeners/CreateDiscourseThreadForEvent.php
+++ app/Listeners/CreateDiscourseThreadForEvent.php
@@ -49,56 +49,56 @@ class CreateDiscourseThreadForEvent
         // Get the user who created the event.
         $users = User::find(EventsUsers::where('event', $partyId)->get());
 
-        if (count($users)) {
+        if (!count($users)) {
             Log::error("Can't find event creator");
-        }
+        } else {
+            $host = $users[0]->user;
 
-        $host = $users[0]->user;
+            try {
+                // We want the host to create the message, so use their username.  The API key should
+                // allow us to do this - see https://meta.discourse.org/t/how-can-an-api-user-create-posts-as-another-user/45968/3.
+                $client = app('discourse-client', [
+                    'username' => $host->username,
+                ]);
 
-        try {
-            // We want the host to create the message, so use their username.  The API key should
-            // allow us to do this - see https://meta.discourse.org/t/how-can-an-api-user-create-posts-as-another-user/45968/3.
-            $client = app('discourse-client', [
-                'username' => $host->username,
-            ]);
+                // See https://meta.discourse.org/t/private-message-send-api/27593/21.
+                $params = [
+                    'raw' => $theParty->free_text,
+                    'title' => $theParty->venue.' '.$theParty->event_date_local,
+                    'target_usernames' => $host->username,
+                    'archetype' => 'private_message',
+                ];
 
-            // See https://meta.discourse.org/t/private-message-send-api/27593/21.
-            $params = [
-                'raw' => $theParty->free_text,
-                'title' => $theParty->venue.' '.$theParty->event_date_local,
-                'target_usernames' => $host->username,
-                'archetype' => 'private_message',
-            ];
+                $endpoint = '/posts.json';
 
-            $endpoint = '/posts.json';
+                Log::info('Creating event thread: '.json_encode($params));
+                $response = $client->request(
+                    'POST',
+                    $endpoint,
+                    [
+                        'form_params' => $params,
+                    ]
+                );
 
-            Log::info('Creating event thread: '.json_encode($params));
-            $response = $client->request(
-                'POST',
-                $endpoint,
-                [
-                    'form_params' => $params,
-                ]
-            );
+                Log::info('Response status: '.$response->getStatusCode());
+                Log::info('Response body: '.$response->getBody());
 
-            Log::info('Response status: '.$response->getStatusCode());
-            Log::info('Response body: '.$response->getBody());
+                if (! $response->getStatusCode() === 200) {
+                    Log::error('Could not create event ('.$partyId.') thread: '.$response->getReasonPhrase());
+                } else {
+                    // We want to save the discourse thread id in the event, so that we can invite people to it later
+                    // when they RSVP.
+                    $json = json_decode($response->getBody(), true);
+                    if (empty($json['topic_id'])) {
+                        throw new \Exception('Topic id not found in create response');
+                    }
 
-            if (! $response->getStatusCode() === 200) {
-                Log::error('Could not create event ('.$partyId.') thread: '.$response->getReasonPhrase());
-            } else {
-                // We want to save the discourse thread id in the event, so that we can invite people to it later
-                // when they RSVP.
-                $json = json_decode($response->getBody(), true);
-                if (empty($json['topic_id'])) {
-                    throw new \Exception('Topic id not found in create response');
+                    $theParty->discourse_thread = $json['topic_id'];
+                    $theParty->save();
                 }
-
-                $theParty->discourse_thread = $json['topic_id'];
-                $theParty->save();
+            } catch (\Exception $ex) {
+                Log::error('Could not create event ('.$partyId.') thread: '.$ex->getMessage());
             }
-        } catch (\Exception $ex) {
-            Log::error('Could not create event ('.$partyId.') thread: '.$ex->getMessage());
         }
     }
 }
