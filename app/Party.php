7d8fda876c823612af3987709cfba518c0d63c9a
Edward Hibbert <edward@ehibbert.org.uk>
2022-04-20 17:12:21 +0100

    Search shouldn't return deleted parties.

--- app/Party.php
+++ app/Party.php
@@ -100,7 +100,7 @@ class Party extends Model implements Auditable
                 FROM `events` AS `e`
                 INNER JOIN `groups` AS `g`
                     ON `g`.`idgroups` = `e`.`group`
-                WHERE `event_end_utc` < NOW()
+                WHERE `event_end_utc` < NOW() AND `deleted_at` IS NULL
                 ORDER BY `e`.`event_start_utc` DESC'));
     }
 
