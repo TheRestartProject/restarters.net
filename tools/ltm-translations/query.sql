3f5b4296baa8216d4e7bedeb0f0a9f8cb1ac2060
zenlan <>
2022-04-23 13:02:57 +0100

    Adds scripts and data for DeepL translation run

---
+++ tools/ltm-translations/query.sql
@@ -0,0 +1,87 @@
+SELECT t1.*,
+'' as trans FROM (
+	SELECT
+	en.`group`,
+	en.`key`,
+	en.`value` as en,
+	fk.`value` as fk,
+	'fr' as 'lang',
+	'fr' as 'alias'
+	FROM `ltm_translations` en
+	LEFT OUTER JOIN `ltm_translations` fk  ON fk.`group` = en.`group` AND fk.`key` = en.`key` AND fk.`locale` = 'fr'
+	WHERE en.`locale` = 'en'
+	AND en.`value` = fk.`value`
+	UNION
+	SELECT
+	en.`group`,
+	en.`key`,
+	en.`value` as en,
+	fk.`value` as fk,
+	'fr-BE' as 'lang',
+	'fr' as 'alias'
+	FROM `ltm_translations` en
+	LEFT OUTER JOIN `ltm_translations` fk  ON fk.`group` = en.`group` AND fk.`key` = en.`key` AND fk.`locale` = 'fr-BE'
+	WHERE en.`locale` = 'en'
+	AND en.`value` = fk.`value`
+	UNION
+	SELECT
+	en.`group`,
+	en.`key`,
+	en.`value` as en,
+	fk.`value` as fk,
+	'de' as 'lang',
+	'de' as 'alias'
+	FROM `ltm_translations` en
+	LEFT OUTER JOIN `ltm_translations` fk  ON fk.`group` = en.`group` AND fk.`key` = en.`key` AND fk.`locale` = 'de'
+	WHERE en.`locale` = 'en'
+	AND en.`value` = fk.`value`
+	UNION
+	SELECT
+	en.`group`,
+	en.`key`,
+	en.`value` as en,
+	fk.`value` as fk,
+	'nl' as 'lang',
+	'nl' as 'alias'
+	FROM `ltm_translations` en
+	LEFT OUTER JOIN `ltm_translations` fk  ON fk.`group` = en.`group` AND fk.`key` = en.`key` AND fk.`locale` = 'nl'
+	WHERE en.`locale` = 'en'
+	AND en.`value` = fk.`value`
+	UNION
+	SELECT
+	en.`group`,
+	en.`key`,
+	en.`value` as en,
+	fk.`value` as fk,
+	'nl-BE' as 'lang',
+	'nl' as 'alias'
+	FROM `ltm_translations` en
+	LEFT OUTER JOIN `ltm_translations` fk  ON fk.`group` = en.`group` AND fk.`key` = en.`key` AND fk.`locale` = 'nl'
+	WHERE en.`locale` = 'en'
+	AND en.`value` = fk.`value`
+	UNION
+	SELECT
+	en.`group`,
+	en.`key`,
+	en.`value` as en,
+	fk.`value` as fk,
+	'it' as 'lang',
+	'it' as 'alias'
+	FROM `ltm_translations` en
+	LEFT OUTER JOIN `ltm_translations` fk  ON fk.`group` = en.`group` AND fk.`key` = en.`key` AND fk.`locale` = 'it'
+	WHERE en.`locale` = 'en'
+	AND en.`value` = fk.`value`
+	UNION
+	SELECT
+	en.`group`,
+	en.`key`,
+	en.`value` as en,
+	fk.`value` as fk,
+	'es' as 'lang',
+	'es' as 'alias'
+	FROM `ltm_translations` en
+	LEFT OUTER JOIN `ltm_translations` fk  ON fk.`group` = en.`group` AND fk.`key` = en.`key` AND fk.`locale` = 'es'
+	WHERE en.`locale` = 'en'
+	AND en.`value` = fk.`value`
+) t1
+ORDER BY t1.lang, t1.`group`, t1.`key`;
---
+++ tools/ltm-translations/readme.txt
@@ -0,0 +1,4 @@
+Run the query in file `query.sql`
+Export to file `todo.csv`
+Enter api key into deeplapi.php
+Execute deeplapi.php
