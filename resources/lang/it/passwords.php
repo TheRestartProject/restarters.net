41f3093bb55739d4f328d4a7d65a1b8991caf4c7
Neil Mather <ngmather@gmail.com>
2022-05-11 08:44:15 +0000

    Update translations from live system

--- resources/lang/it/passwords.php
+++ resources/lang/it/passwords.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-  'password' => 'Le password devono contenere almeno sei caratteri e corrispondere alla conferma.',
   'reset' => 'La tua password e\' stata resettata!',
   'sent' => 'Ti abbiamo inviato una email contentente un link per reimpostarti la password!',
   'token' => 'Questo token di reimpostazione della password non è valido.',
   'user' => 'Non troviamo alcun utente con quell\'indirizzo email',
+  'password' => 'Le password devono contenere almeno sei caratteri e corrispondere alla conferma.',
 ];
