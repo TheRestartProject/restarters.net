dc61dc713c22c6f71eef0aabfe2cb806171df637
Edward Hibbert <edward@ehibbert.org.uk>
2022-05-11 11:56:04 +0100

    Playwright test - approve event

--- tests/Integration/event.test.js
+++ tests/Integration/event.test.js
@@ -1,8 +1,9 @@
 const {test, expect} = require('@playwright/test')
-const { login, createGroup, createEvent } = require('./utils')
+const { login, createGroup, createEvent, approveEvent} = require('./utils')
 
 test('Can create event', async ({page, baseURL}) => {
   await login(page, baseURL)
   const groupid = await createGroup(page, baseURL)
   const eventid = await createEvent(page, baseURL, groupid)
+  await approveEvent(page, baseURL, eventid)
 })
