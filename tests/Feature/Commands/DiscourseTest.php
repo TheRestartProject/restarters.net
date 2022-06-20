213ca61be3e20a35e745a0147d49ec2794c4b239
Edward Hibbert <edward@ehibbert.org.uk>
2022-06-20 17:21:54 +0100

    Add tests for scheduled commands.

---
+++ tests/Feature/Commands/DiscourseTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Commands;
+
+use DB;
+use Tests\TestCase;
+
+class DiscourseTest extends TestCase {
+    public function testSyncDiscourseUsernames() {
+        $this->artisan('sync:discourseusernames')->assertExitCode(0);
+    }
+
+    public function testLanguageSync() {
+        $this->artisan('language:sync')->assertExitCode(0);
+    }
+
+    public function testDiscourseSyncGroups() {
+        $this->artisan('discourse:syncgroups')->assertExitCode(0);
+    }
+}
