be82228a5df63124659db25e878b8f4fb9569fa8
edwh <edward@ehibbert.org.uk>
2022-06-28 11:04:20 +0100

    Upgrade packages

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,14 +1,6 @@
 let mix = require('laravel-mix');
 let webpack = require('webpack');
 const WebpackShellPlugin = require('webpack-shell-plugin');
-require('laravel-mix-bundle-analyzer');
-
-if (!mix.inProduction()) {
-    mix.bundleAnalyzer({
-        analyzerMode: 'static',
-        openAnalyzer: false
-    });
-}
 
 mix.webpackConfig({
     plugins: [
