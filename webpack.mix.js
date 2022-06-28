b595cf707b9f969d9509ef0196a9b7ebd3067ca9
edwh <edward@ehibbert.org.uk>
2022-06-28 14:15:00 +0100

    Build fixes

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,31 +1,21 @@
 let mix = require('laravel-mix');
 let webpack = require('webpack');
-const WebpackShellPlugin = require('webpack-shell-plugin');
+const WebpackShellPluginNext = require('webpack-shell-plugin-next');
 
 mix.webpackConfig({
-    plugins: [
-        new webpack.IgnorePlugin(/^codemirror$/),
-        // Build a JS translation file that corresponds to our PHP lang/ folder.
-        new WebpackShellPlugin({onBuildStart:['php artisan lang:js --no-lib --quiet resources/js/translations.js'], onBuildEnd:[]})
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
     ]
+  },
+  plugins: [
+    // Build a JS translation file that corresponds to our PHP lang/ folder.
+    new WebpackShellPluginNext({onBuildStart:['php artisan lang:js --no-lib --quiet resources/js/translations.js'], onBuildEnd:[]})
+  ]
 });
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-// mix.scripts([
-//     'node_modules/js-cookie/src/js.cookie.js',
-//     'resources/js/gdpr-cookie-notice/templates.js',
-//     'resources/js/gdpr-cookie-notice/script.js',
-//     'resources/js/gdpr-cookie-notice/en.js'
-// ], 'public/js/gdpr-cookie-notice.js');
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
