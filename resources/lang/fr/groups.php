494150f31a87330ac98b2ec03099ed755fca7176
Edward Hibbert <edward@ehibbert.org.uk>
2022-08-10 09:15:33 +0100

    Missing translations.

--- resources/lang/fr/groups.php
+++ resources/lang/fr/groups.php
@@ -159,5 +159,5 @@ return [
   'talk_group_add_body' => 'Merci de suivre le groupe :group_name ! Vous recevrez désormais des notifications lorsque de nouveaux événements seront planifiés et seront ajoutés aux messages de groupe. <a href="https://talk.restarters.net/t/how-to-communicate-with-your-repair-group/6293">Découvrez comment fonctionnent les messages de groupe et comment modifier vos paramètres de notification</a>.',
   'you_have_joined' => 'Vous avez rejoint <a class="plain-link" href=":url">:name</a>',
   'moderation_none' => 'Il n\'y a actuellement aucun Repair Cafés à modérer',
-  'groups_title_admin' => 'Repair Cafés to moderate',
+  'groups_title_admin' => 'Repair Cafés à modérer',
 ];
