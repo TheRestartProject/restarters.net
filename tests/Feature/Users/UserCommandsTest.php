f3ffd74ae11ee50a3deb6c5a187d75c4dd71d08e
Edward Hibbert <edward@ehibbert.org.uk>
2022-10-10 11:26:36 +0100

    Test fixes.

--- tests/Feature/Users/UserCommandsTest.php
+++ tests/Feature/Users/UserCommandsTest.php
@@ -4,6 +4,7 @@ namespace Tests\Commands;
 
 use App\Group;
 use App\Helpers\Fixometer;
+use App\Network;
 use App\User;
 use DB;
 use Tests\TestCase;
@@ -13,9 +14,14 @@ use function PHPUnit\Framework\assertTrue;
 
 class UserCommandsTest extends TestCase {
     public function testCreate() {
-        $this->artisan('user:create testname test@test.com 1234567890')->assertExitCode(0);
+        $network = Network::all()->first();
+        DB::connection()->enableQueryLog();
+        $this->artisan('user:create testname test@test.com 1234567890 fr ' . $network->id)->assertExitCode(0);
         $user = User::where('email', 'test@test.com')->first();
+        $queries = DB::getQueryLog();
         self::assertEquals('testname', $user->name);
+        self::assertEquals('fr', $user->language);
+        self::assertEquals($network->id, $user->repair_network);
     }
 
     public function testMakeHost() {
