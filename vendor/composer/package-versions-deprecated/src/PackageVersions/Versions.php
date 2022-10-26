70c43d0bb90df40bb3f37587cd54a534b3ddf6f1
edwh <edward@therestartproject.org>
2022-10-26 10:32:25 +0000

    Build assets

--- vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
+++ vendor/composer/package-versions-deprecated/src/PackageVersions/Versions.php
@@ -148,7 +148,7 @@ final class Versions
   'weotch/phpthumb' => '1.0.5@f5c1d2ef2e9de3bded7483778618bf8cac2511c3',
   'wouternl/laravel-drip' => '1.2.4@f9d96140ba62f4ddb4df909e20931e897e0edd54',
   'zircote/swagger-php' => '3.3.6@5016342f966fca29dda84455de066c5c90d37941',
-  'laravel/laravel' => 'dev-production@1e8fb610dda4126cb75862a28784715893bd4019',
+  'laravel/laravel' => 'dev-production@ea1222c87538e5997eb683cf9d8cc6c3542d6aaa',
 );
 
     private function __construct()
