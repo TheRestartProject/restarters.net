105e13bf6d527c2b2359218bf238ce4dd76673ae
Shift <shift@laravelshift.com>
2022-12-01 14:25:59 +0000

    Shift core files

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -110,4 +110,15 @@ class EventServiceProvider extends ServiceProvider
             Event::listen('Illuminate\Auth\Events\Login', \App\Listeners\LogInToWiki::class);
         }
     }
+
+    /**
+     * Determine if events and listeners should be automatically discovered.
+     *
+     * @return bool
+     */
+    public function shouldDiscoverEvents()
+    {
+        return false;
+    }
 }
+
