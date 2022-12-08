b96cb55cb458beaa56f2b7fae113dba98336535f
Edward Hibbert <edward@ehibbert.org.uk>
2022-12-08 10:05:04 +0000

    Ensure age is not older than 500 years.

--- resources/js/store/devices.js
+++ resources/js/store/devices.js
@@ -131,6 +131,12 @@ export default {
         headers: {
           'X-CSRF-TOKEN': rootGetters['auth/CSRF']
         }
+      }).catch(function(error) {
+        if (error && error.response && error.response.data) {
+          throw new Error(error.response.data.message)
+        } else {
+          throw new Error('Unknown error')
+        }
       })
 
       if (ret && ret.data && ret.data.success && ret.data.devices) {
@@ -159,6 +165,12 @@ export default {
       let ret = await axios.post('/device/edit/' + params.iddevices, params, {
         headers: {
           'X-CSRF-TOKEN': rootGetters['auth/CSRF']
+        }
+      }).catch(function(error) {
+        if (error && error.response && error.response.data) {
+          throw new Error(error.response.data.message)
+        } else {
+          throw new Error('Unknown error')
         }
       })
 
