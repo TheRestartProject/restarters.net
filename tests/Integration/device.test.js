a61f139a5e1d4aae4fcc2de5692ccea7b1212ef2
Edward Hibbert <edward@ehibbert.org.uk>
2022-12-12 12:34:23 +0000

    Testing Coveralls code coverage

--- tests/Integration/device.test.js
+++ tests/Integration/device.test.js
@@ -1,7 +1,5 @@
 const { test, expect } = require("playwright-test-coverage");
 const { login, createGroup, createEvent, approveEvent, addDevice } = require('./utils')
-const v8toIstanbul = require('v8-to-istanbul');
-const fs = require('file-system');
 
 test('Spare parts set as expected', async ({page, baseURL}) => {
   test.slow()
@@ -17,17 +15,6 @@ test('Spare parts set as expected', async ({page, baseURL}) => {
 
   // Should  see spare parts tick in summary.  Two copies because of mobile view.
   await expect(await page.locator('.spare-parts-tick').count()).toEqual(2);
-
-  const coverage = await page.coverage.stopJSCoverage();
-  console.log('Got coverage')
-  for (const entry of coverage) {
-    const fn = entry.url.replace(/[^a-z0-9]/gi, '_').toLowerCase();
-    console.log(fn);
-    const converter = v8toIstanbul('', 0, { source: entry.source });
-    await converter.load();
-    converter.applyCoverage(entry.functions);
-    await fs.writeFile(fn + '.json', JSON.stringify(converter.toIstanbul()));
-  }
 })
 
 test('Spare parts not set unexpectedly', async ({page, baseURL}) => {
