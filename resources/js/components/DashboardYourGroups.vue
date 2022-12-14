03283e23e6715f87ee0e6e79e9769532410077e6
Edward Hibbert <edward@ehibbert.org.uk>
2022-12-14 13:19:46 +0000

    Scoped style of 'layout' is being inherited by a child component which also has a scoped style of the same name.
    Exception if can't log in to Mediawiki.

--- resources/js/components/DashboardYourGroups.vue
+++ resources/js/components/DashboardYourGroups.vue
@@ -23,7 +23,7 @@
             <b-img src="/images/arrow-right-doodle-white.svg" />
             {{ translatedNewlyAdded }}
           </a>
-          <div class="layout">
+          <div class="dyg-layout">
             <div class="group-intro">
               <h3>
                 {{ __('dashboard.groups_heading') }}
@@ -157,7 +157,7 @@ h3 {
   text-decoration: underline;
 }
 
-.layout {
+.dyg-layout {
   display: grid;
   grid-template-columns: 1fr;
   grid-template-rows: auto auto auto 40px auto auto auto;
