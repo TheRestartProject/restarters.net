c90f46c86526da5c42f8d9c61fab7495a8cdd990
Edward Hibbert <edward@ehibbert.org.uk>
2023-01-13 18:37:28 +0000

    WIP controller translation

---
+++ lang/en/group-tags.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'create_success' => 'Group Tag successfully created!',
+    'update_success' => 'Group Tag successfully updated!',
+    'delete_success' => 'Brand deleted!',
+    'title' => 'Group Tags',
+    'edit_tag' => 'Edit Group Tag',
+];
