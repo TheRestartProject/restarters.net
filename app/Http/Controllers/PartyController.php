1cccfa1c47073dafcc54a2ca6b9dd8d4c35d1735
Edward Hibbert <edward@ehibbert.org.uk>
2023-02-13 15:23:33 +0000

    Use eager-loading to reduce N+1 queries and speed things up.  Also cache Roles.

--- app/Http/Controllers/PartyController.php
+++ app/Http/Controllers/PartyController.php
@@ -52,7 +52,7 @@ class PartyController extends Controller
         if (is_null($group)) {
             // We are showing events for multiple groups and so we need to pass the relevant group, in order that
             // we can show the group name and link to it.
-            $thisone['group'] = \App\Group::find($event->group);
+            $thisone['group'] = \App\Group::with('networks')->find($event->group);
         }
 
         $thisone['attending'] = Auth::user() && $event->isBeingAttendedBy(Auth::user()->id);
@@ -132,7 +132,7 @@ class PartyController extends Controller
             }
 
             // ...and any other upcoming approved events
-            $other_upcoming_events = Party::future()->
+            $other_upcoming_events = Party::with('theGroup.networks')->future()->
                 whereNotIn('idevents', \Illuminate\Support\Arr::pluck($events, 'idevents'))->
                 get();
 
@@ -373,7 +373,8 @@ class PartyController extends Controller
             $images = null;
         }
 
-        $allGroups = Group::orderBy('name')->get();
+        // Fetch the nextwork here - avoids fetching for each group as we encode.
+        $allGroups = Group::with('networks')->orderBy('name')->get();
 
         if ($request->isMethod('post') && ! empty($request->post())) {
             $id = $request->post('id');
