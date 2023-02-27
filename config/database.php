9078e723c36784e31a1f4fd7e3b49d893ffc7d92
Edward Hibbert <edward@ehibbert.org.uk>
2023-02-27 14:56:31 +0000

    WIP Platform.sh

--- config/database.php
+++ config/database.php
@@ -3,9 +3,12 @@
 use Illuminate\Support\Str;
 
 $config = new Platformsh\ConfigReader\Config();
-$rels = json_decode(getenv("PLATFORM_RELATIONSHIPS"), true);
 
-error_log(var_export($rels, true));
+if (!$config->isValidPlatform()) {
+    die("Not in a Platform.sh Environment.");
+}
+
+$creds = $config->credentials('database');
 
 return [
 
@@ -50,13 +53,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'database' => $creds['path'],
+            'username' => $creds['username'],
+            'password' => $creds['password'],
+            'host' => $creds['host'],
+            'port' => $creds['port'],
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
@@ -67,6 +68,7 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_COMPRESS => !empty($creds['query']['compression'])
             ]) : [],
         ],
 
