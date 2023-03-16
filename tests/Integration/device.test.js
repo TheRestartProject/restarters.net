cf88b6c92e5fab700031b0ef8d103495838655d8
Edward Hibbert <edward@ehibbert.org.uk>
2023-03-16 15:55:08 +0000

    WIP visual tests.

--- tests/Integration/device.test.js
+++ tests/Integration/device.test.js
@@ -1,5 +1,6 @@
 const {test, expect} = require('@playwright/test')
 const { login, createGroup, createEvent, approveEvent, addDevice } = require('./utils')
+import faker from 'faker';
 
 test('Spare parts set as expected', async ({page, baseURL}) => {
   // Need to set faker seed so that we get the same data each time for the screenshot.
@@ -7,15 +8,15 @@ test('Spare parts set as expected', async ({page, baseURL}) => {
 
   test.slow()
   await login(page, baseURL)
-  expect(await page.screenshot()).toMatchSnapshot();
+  expect(await page.screenshot()).toMatchSnapshot({threshold:0.05});
   const groupid = await createGroup(page, baseURL)
-  expect(await page.screenshot()).toMatchSnapshot();
+  expect(await page.screenshot()).toMatchSnapshot({threshold:0.05});
   const eventid = await createEvent(page, baseURL, groupid, true)
-  expect(await page.screenshot()).toMatchSnapshot();
+  expect(await page.screenshot()).toMatchSnapshot({threshold:0.05});
   await approveEvent(page, baseURL, eventid)
-  expect(await page.screenshot()).toMatchSnapshot();
+  expect(await page.screenshot()).toMatchSnapshot({threshold:0.05});
   await addDevice(page, baseURL, eventid, true, false, true, true)
-  expect(await page.screenshot()).toMatchSnapshot();
+  expect(await page.screenshot()).toMatchSnapshot({threshold:0.05});
 
   // Should  see spare parts tick in summary.  Two copies because of mobile view.
   await expect(await page.locator('.spare-parts-tick').count()).toEqual(2);
--- tests/Integration/landingpage.test.js
+++ tests/Integration/landingpage.test.js
@@ -1,6 +1,6 @@
 const {test, expect} = require('@playwright/test')
 const { login } = require('./utils')
-import faker from '@faker-js/faker';
+import faker from 'faker';
 
 test('Landing page has blurb', async ({page, baseURL}) => {
   // Need to set faker seed so that we get the same data each time for the screenshot.
@@ -11,7 +11,7 @@ test('Landing page has blurb', async ({page, baseURL}) => {
   await page.goto(baseURL)
   const legend = page.locator('h2').first()
   await expect(legend).toHaveText('Learn and share repair skills with others')
-  expect(await page.screenshot()).toMatchSnapshot();
+  expect(await page.screenshot()).toMatchSnapshot({threshold:0.05});
 })
 
 test('Can log in', async({page, baseURL}) => {
