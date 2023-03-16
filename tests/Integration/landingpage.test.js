51d10c35cee4f8ef600c8be89d8d13f5f7ffdbf4
Edward Hibbert <edward@ehibbert.org.uk>
2023-03-16 14:10:45 +0000

    WIP visual testing

--- tests/Integration/landingpage.test.js
+++ tests/Integration/landingpage.test.js
@@ -7,6 +7,7 @@ test('Landing page has blurb', async ({page, baseURL}) => {
   await page.goto(baseURL)
   const legend = page.locator('h2').first()
   await expect(legend).toHaveText('Learn and share repair skills with others')
+  expect(await page.screenshot()).toMatchSnapshot();
 })
 
 test('Can log in', async({page, baseURL}) => {
