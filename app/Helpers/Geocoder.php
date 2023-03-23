28aab7c0691a49866ba11beb8306d484107fde4a
Edward Hibbert <edward@ehibbert.org.uk>
2023-03-23 16:42:31 +0000

    Test fixes.

--- app/Helpers/Geocoder.php
+++ app/Helpers/Geocoder.php
@@ -11,7 +11,7 @@ class Geocoder
     public function geocode($location)
     {
         if ($location != 'ForceGeocodeFailure') {
-            $geocodeResponse = app('geocoder')->geocodeQuery(GeocodeQuery::create($location)->withData('location_type', Mapbox::TYPE_ADDRESS));
+            $geocodeResponse = app('geocoder')->geocodeQuery(GeocodeQuery::create($location)->withData('location_type', Mapbox::TYPE_PLACE));
             $addressCollection = $geocodeResponse->get();
             $address = $addressCollection->get(0);
             if ($address) {
