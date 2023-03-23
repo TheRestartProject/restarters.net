9dc224181c30138b1632efb1e0c4b545ad97f0c5
Edward Hibbert <edward@ehibbert.org.uk>
2023-03-23 16:10:49 +0000

    Change server to use Mapbox geocode.

---
+++ tests/Unit/t/GeocoderTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit\t;
+
+use Tests\TestCase;
+
+class GeocoderTest extends TestCase
+{
+    public function testGeocode() {
+        $geocoder = new \App\Helpers\Geocoder();
+        $ret = $geocoder->geocode('6 Canterbury Crescent, London SW9 7QD');
+        $this->assertEquals($ret['latitude'], 51.46398);
+        $this->assertEquals($ret['longitude'], -0.11302);
+        $this->assertEquals($ret['country'], 'United Kingdom');
+    }
+}
