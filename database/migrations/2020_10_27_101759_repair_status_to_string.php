74f11217d8e850897c80fb014f9728cf16c11d3d
Edward Hibbert <edward@ehibbert.org.uk>
2023-04-11 14:49:56 +0100

    Revert "Remove CREATE FUNCTION which fails in MySQL 8 and isn't actually used."
    This reverts commit 1d827646e21e4e4dd5d791ee3b72c59ab85aa44a.

---
+++ database/migrations/2020_10_27_101759_repair_status_to_string.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared('DROP FUNCTION IF EXISTS `REPAIR_STATUS_TO_STRING`');
+        // Might need to set definer
+//        DB::unprepared("CREATE DEFINER='root'@'localhost' FUNCTION `REPAIR_STATUS_TO_STRING`(`id` INT)
+        DB::unprepared("CREATE FUNCTION `REPAIR_STATUS_TO_STRING`(`id` INT)
+RETURNS varchar(12) CHARSET utf8
+    NO SQL
+    DETERMINISTIC
+    SQL SECURITY INVOKER
+BEGIN
+  DECLARE res VARCHAR(12);
+  CASE id
+   WHEN 1 THEN SET res = 'Fixed';
+   WHEN 2 THEN SET res = 'Repairable';
+   WHEN 3 THEN SET res = 'End of life';
+   ELSE SET res = 'Unknown';
+  END CASE;
+  RETURN res;
+END
+");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared('DROP FUNCTION IF EXISTS `REPAIR_STATUS_TO_STRING`');
+    }
+};
