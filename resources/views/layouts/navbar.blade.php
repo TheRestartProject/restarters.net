3242a386dbb93a2044e80f33ed6e86aeda1f9bfd
Edward Hibbert <edward@ehibbert.org.uk>
2023-06-21 09:57:09 +0100

    Fix branch name display.

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -17,8 +17,8 @@
         <?php
         // We want to show the current branch.  This will only be set on development or staging environments.
         $branch = "Unknown branch";
-        if (is_dir('.git')) {
-            @exec('git branch | ' . "grep ' * '", $shellOutput);
+        if (is_dir(base_path() . '/.git')) {
+            exec('cd ' . base_path() . '; git branch | ' . "grep ' * '", $shellOutput);
             foreach ($shellOutput as $line) {
                 if (strpos($line, '* ') !== false) {
                     $branch = trim(strtolower(str_replace('* ', '', $line)));
