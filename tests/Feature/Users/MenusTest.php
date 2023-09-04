043e29c2a73988e29ecbfa2542b699135b500a7b
Edward Hibbert <edward@ehibbert.org.uk>
2023-09-04 16:06:54 +0100

    Improve testing of menus

--- tests/Feature/Users/MenusTest.php
+++ tests/Feature/Users/MenusTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Events\UserUpdated;
 use App\User;
+use App\UsersPermissions;
 use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Event;
@@ -22,6 +23,17 @@ class MenusTest extends TestCase
                     1 => 'Reporting',
                     2 => 'General',
                 ],
+                true,
+                [
+                    0 => 'Brands',
+                    1 => 'Skills',
+                    2 => 'Group tags',
+                    3 => 'Categories',
+                    4 => 'Users',
+                    5 => 'Roles',
+                    6 => 'Networks',
+                    7 => 'Translations',
+                ]
             ],
             [
                 'NetworkCoordinator',
@@ -29,6 +41,11 @@ class MenusTest extends TestCase
                     0 => 'Administrator',
                     1 => 'General',
                 ],
+                true,
+                [
+                    0 => 'Translations',
+                    1 => 'Networks'
+                ]
             ],
             [
                 'Host',
@@ -36,12 +53,16 @@ class MenusTest extends TestCase
                     0 => 'Reporting',
                     1 => 'General',
                 ],
+                false,
+                []
             ],
             [
                 'Restarter',
                 [
                     0 => 'General',
                 ],
+                false,
+                []
             ],
         ];
     }
@@ -49,15 +70,27 @@ class MenusTest extends TestCase
     /**
      *@dataProvider provider
      */
-    public function testSections($role, $present)
+    public function testSections($role, $present, $translator, $adminMenu)
     {
         $user = User::factory()->{lcfirst($role)}()->create();
+
+        if ($translator) {
+            $up = new UsersPermissions();
+            $up->user_id = $user->id;
+            $up->permission_id = 6;
+            $up->save();
+        }
+
         $this->actingAs($user);
 
         $response = $this->get('/user/menus');
         $menus = json_decode($response->getContent(), true);
 
         $this->assertEquals($present, array_keys($menus));
+
+        if ($adminMenu && count($adminMenu)) {
+            $this->assertEquals($adminMenu, array_keys($menus['Administrator']['items']));
+        }
     }
 
     public function testLoggedOut()
