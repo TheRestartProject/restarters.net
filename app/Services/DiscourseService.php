076d8fb2ac63ba8c6432b9e0ec2659f76b8a824c
Edward Hibbert <edward@ehibbert.org.uk>
2023-11-21 15:40:36 +0000

    Revert "Discourse API request for owner addition/removal is wrong.  Not clear whether it has always been wrong or has changed with Communiteq upgrade."
    This reverts commit 88823ea45a2e98bf1d8e8a2400263a183b8d350e.

--- app/Services/DiscourseService.php
+++ app/Services/DiscourseService.php
@@ -407,7 +407,7 @@ class DiscourseService
 
                             if ($d['owner'] && !$shouldBeOwner) {
                                 Log::info("Remove $discourseMember as admin of {$discourseId} {$discourseName}");
-                                $response = $client->request('DELETE', "/groups/$discourseId/owners.json", [
+                                $response = $client->request('DELETE', "/admin/groups/$discourseId/owners.json", [
                                     'form_params' => [
                                         'user_id' => $d['id']
                                     ]
@@ -423,7 +423,7 @@ class DiscourseService
                                 }
                             } else if (!$d['owner'] && $shouldBeOwner) {
                                 Log::info("Add $discourseMember as admin of {$discourseId} {$discourseName}");
-                                $response = $client->request('PUT', "/groups/$discourseId/owners.json", [
+                                $response = $client->request('PUT', "/admin/groups/$discourseId/owners.json", [
                                     'form_params' => [
                                         'group' => [
                                             'usernames' => $discourseMember
