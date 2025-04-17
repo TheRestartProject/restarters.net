55bb8b020ecf52193258bd4bec17fd87c60ea665
Angel de la Torre <angel.dlt.1996@gmail.com>
2025-04-17 13:24:19 -0700

    refactor(docker): remove env overrides in docker_run.sh
    Instead of modifying the .env file within docker_run.sh, we will just
    use the .env as is. The phpunit.xml modifications also do not currently
    have any effect so we will remove them for now as we try to get just a
    barebones development environment working.

--- docker_run.sh
+++ docker_run.sh
@@ -12,23 +12,6 @@ fi
 rm -rf vendor
 composer install
 
-# Point at the DB server
-sed -i 's/DB_HOST=.*$/DB_HOST=restarters_db/g' .env
-sed -i 's/DB_DATABASE=.*$/DB_DATABASE=restarters_db_test/g' .env
-
-# Turn off session domain, which causes problems in a Docker environment.
-sed -i 's/SESSION_DOMAIN=.*$/SESSION_DOMAIN=/g' .env
-
-# Change the Discourse host to point at the one defined in docker-compose
-sed -i 's/DISCOURSE_URL=.*$/DISCOURSE_URL=http:\/\/restarters_discourse:80/g' .env
-
-# Change the Discourse secret to be the value we set up in Discourse itself.
-sed -i 's/DISCOURSE_SECRET=.*$/DISCOURSE_SECRET=mustbetencharacters/g' .env
-
-# Change the database environment used for automated tests.
-sed -i 's/SESSION_DOMAIN=.*$/SESSION_DOMAIN=/g' phpunit.xml
-sed -i 's/DB_TEST_HOST=.*$/DB_TEST_HOST=restarters_db/g' phpunit.xml
-
 mkdir storage/framework/cache/data
 php artisan migrate
 npm install --legacy-peer-deps
