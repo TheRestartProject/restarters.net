25614d30d74280c67eb0ce3f2553d9d9e5fcffbe
Shift <shift@laravelshift.com>
2025-04-22 20:57:55 +0000

    Adopt anonymous migrations

--- database/migrations/2025_04_21_192728_remove_drip_columns.php
+++ database/migrations/2025_04_21_192728_remove_drip_columns.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class RemoveDripColumns extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -37,4 +37,4 @@ class RemoveDripColumns extends Migration
             $table->boolean('users_push_to_drip')->nullable(false)->default(false);
         });
     }
-}
+};
