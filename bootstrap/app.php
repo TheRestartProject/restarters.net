89c8db48f77b1abb0d4cf998d1925cf418e0291c
Shift <shift@laravelshift.com>
2025-04-22 20:57:46 +0000

    Re-register HTTP middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,64 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectGuestsTo(fn () => route('login'));
+        $middleware->redirectUsersTo(RouteServiceProvider::HOME);
+
+        $middleware->encryptCookies(except: [
+            'UseCDNCache',
+            'UseDC',
+            'wiki_db_mw__session',
+            'wiki_db_mw_Token',
+            'wiki_db_mw_UserID',
+            'wiki_db_mw_UserName',
+            'wiki_test_session',
+            'wiki_testToken',
+            'wiki_testUserID',
+            'wiki_testUserName',
+            'wiki_devToken',
+            'wiki_devUserID',
+            'wiki_devUserName',
+            'wiki_dev_mw__session',
+            'wiki_dev_mw_Token',
+            'wiki_dev_mw_UserID',
+            'wiki_dev_mw_UserName',
+            'authenticated',
+            'restarters_apitoken'
+        ]);
+
+        $middleware->append(\App\Http\Middleware\HttpsProtocol::class);
+
+        $middleware->web([
+            \App\Http\Middleware\CheckForRepairNetwork::class,
+            \App\Http\Middleware\LanguageSwitcher::class,
+            \App\Http\Middleware\LogHTTPErrorsToSentry::class,
+        ]);
+
+        $middleware->throttleApi();
+
+        $middleware->group('translation', [
+            \App\Http\Middleware\VerifyTranslationAccess::class,
+        ]);
+
+        $middleware->alias([
+            'AcceptUserInvites' => \App\Http\Middleware\AcceptUserInvites::class,
+            'ensureAPIToken' => \App\Http\Middleware\EnsureAPIToken::class,
+            'localeSessionRedirect' => \Mcamara\LaravelLocalization\Middleware\LocaleSessionRedirect::class,
+            'localeViewPath' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationViewPath::class,
+            'localizationRedirect' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRedirectFilter::class,
+            'localize' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRoutes::class,
+            'verifyUserConsent' => \App\Http\Middleware\VerifyUserConsent::class,
+        ]);
+
+        $middleware->priority([
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \App\Http\Middleware\Authenticate::class,
+            \Illuminate\Routing\Middleware\ThrottleRequests::class,
+            \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Auth\Middleware\Authorize::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
