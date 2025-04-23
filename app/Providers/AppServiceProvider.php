4c3c1e23a8bd6eeb6aea58fbb475bc597939532f
Angel de la Torre <angel.dlt.1996@gmail.com>
2025-04-23 10:54:03 -0700

    fix(events): finish consolidating event handling in AppServiceProvider
    Laravel 11 introduced a different standard for handling events. As such,
    we need to move all the event logic out of the EventServiceProvider and
    into the AppServiceProvider.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,7 @@ use App\Helpers\RobustTranslator;
 use App\Observers\EventsUsersObserver;
 use Auth;
 use Cache;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Translation\Translator;
 use OwenIt\Auditing\Models\Audit;
@@ -47,6 +48,8 @@ class AppServiceProvider extends ServiceProvider
         \Illuminate\Pagination\Paginator::useBootstrapThree();
 
         EventsUsers::observe(EventsUsersObserver::class);
+        
+        $this->registerEvents();
     }
 
     /**
@@ -69,5 +72,69 @@ class AppServiceProvider extends ServiceProvider
 
         // Register exception handler (recommended for Laravel 11)
         $this->app->bind(\Illuminate\Contracts\Debug\ExceptionHandler::class, \App\Exceptions\Handler::class);
+    }
+    
+    /**
+     * Register all events
+     */
+    protected function registerEvents(): void
+    {
+        // Login events
+        Event::listen('Illuminate\Auth\Events\Login', \App\Listeners\LogSuccessfulLogin::class);
+        
+        // Event approval events
+        Event::listen(\App\Events\ApproveEvent::class, [
+            \App\Listeners\CreateWordpressPostForEvent::class,
+            \App\Listeners\CreateDiscourseThreadForEvent::class,
+            \App\Listeners\NotifyApprovedEvent::class
+        ]);
+        
+        // Event edit events
+        Event::listen(\App\Events\EditEvent::class, \App\Listeners\EditWordpressPostForEvent::class);
+        
+        // Event deleted events
+        Event::listen(\App\Events\EventDeleted::class, \App\Listeners\DeleteEventFromWordPress::class);
+        
+        // Group approval events
+        Event::listen(\App\Events\ApproveGroup::class, [
+            \App\Listeners\CreateWordpressPostForGroup::class,
+            \App\Listeners\CreateDiscourseGroupForGroup::class
+        ]);
+        
+        // Group edit events
+        Event::listen(\App\Events\EditGroup::class, \App\Listeners\EditWordpressPostForGroup::class);
+        
+        // Password events
+        Event::listen(\App\Events\PasswordChanged::class, \App\Listeners\ChangeWikiPassword::class);
+        
+        // User events
+        Event::listen(\App\Events\UserUpdated::class, \App\Listeners\SyncUserProperties::class);
+        Event::listen(\App\Events\UserFollowedGroup::class, \App\Listeners\AddUserToDiscourseGroup::class);
+        Event::listen(\App\Events\UserDeleted::class, [
+            \App\Listeners\RemoveSoftDeletedUserFromAllGroups::class,
+            \App\Listeners\SendAdminUserDeletedNotification::class,
+            \App\Listeners\AnonymiseSoftDeletedUser::class
+        ]);
+        
+        // Event images events
+        Event::listen(\App\Events\EventImagesUploaded::class, \App\Listeners\SendAdminModerateEventPhotosNotification::class);
+        
+        // Logout events
+        Event::listen(\Illuminate\Auth\Events\Logout::class, \App\Listeners\LogOutOfWiki::class);
+        
+        // Device events
+        Event::listen(\App\Events\DeviceCreatedOrUpdated::class, \App\Listeners\DeviceUpdatedAt::class);
+        
+        // User event confirmation events
+        Event::listen(\App\Events\UserConfirmedEvent::class, \App\Listeners\AddUserToDiscourseThreadForEvent::class);
+        Event::listen(\App\Events\UserLeftEvent::class, \App\Listeners\RemoveUserFromDiscourseThreadForEvent::class);
+        
+        // Subscribe to event subscribers
+        Event::subscribe(\App\Listeners\DiscourseUserEventSubscriber::class);
+        
+        // Feature-specific events
+        if (env('FEATURE__WIKI_INTEGRATION') === true) {
+            Event::listen('Illuminate\Auth\Events\Login', \App\Listeners\LogInToWiki::class);
+        }
     }
 }
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,7 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\EventServiceProvider::class,
     App\Providers\MediawikiServiceProvider::class,
     App\Providers\WordpressServiceProvider::class,
     App\Providers\AgentServiceProvider::class,
