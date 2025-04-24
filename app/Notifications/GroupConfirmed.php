7d0590e4f3aa8683af9bfe3aab3fa90b860ed689
Angel de la Torre <angel.dlt.1996@gmail.com>
2025-04-24 10:48:57 -0700

    fix(types): set correct return types for event and notification classes
    This was a cleanup commit to address Laravel Shift changes.

--- app/Notifications/GroupConfirmed.php
+++ app/Notifications/GroupConfirmed.php
@@ -22,6 +22,7 @@ class GroupConfirmed extends BaseNotification
      * Get the notification's delivery channels.
      *
      * @param mixed $notifiable
+     * @return array
      */
     public function via($notifiable): array
     {
@@ -32,6 +33,7 @@ class GroupConfirmed extends BaseNotification
      * Get the mail representation of the notification.
      *
      * @param mixed $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
      */
     public function toMail($notifiable): MailMessage
     {
@@ -57,6 +59,7 @@ class GroupConfirmed extends BaseNotification
      * Get the array representation of the notification.
      *
      * @param mixed $notifiable
+     * @return array
      */
     public function toArray($notifiable): array
     {
--- app/Notifications/JoinEvent.php
+++ app/Notifications/JoinEvent.php
@@ -10,6 +10,7 @@ class JoinEvent extends BaseNotification
      * Get the notification's delivery channels.
      *
      * @param mixed $notifiable
+     * @return array
      */
     public function via($notifiable): array
     {
@@ -40,6 +41,7 @@ class JoinEvent extends BaseNotification
      * Get the mail representation of the notification.
      *
      * @param mixed $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
      */
     public function toMail($notifiable): MailMessage
     {
@@ -129,6 +131,7 @@ class JoinEvent extends BaseNotification
      * Get the array representation of the notification.
      *
      * @param mixed $notifiable
+     * @return array
      */
     public function toArray($notifiable): array
     {
