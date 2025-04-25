3178d4d4c22eb79082b2fa6d7822cdc51891b95e
Angel de la Torre <angel.dlt.1996@gmail.com>
2025-04-25 14:20:45 -0700

    fix(events): add __invoke method to BaseEvent
    This issue was found while running tests.
    
    The core issue is that Laravel 12 changed the way queued events are handled.
    In Laravel 12, the queue system expects listener classes to implement
    an __invoke() method when processing queued events, which is a change
    from earlier versions where handle() was called directly.
    
    The error "Call to undefined method __invoke()" was occurring because
    our BaseEvent class and its children didn't implement this method.
    
    This was probably missed during our Laravel 9-12 upgrades since we were
    not able to run tests locally.

--- app/Listeners/BaseEvent.php
+++ app/Listeners/BaseEvent.php
@@ -8,4 +8,18 @@ use Illuminate\Bus\Queueable;
 
 abstract class BaseEvent implements ShouldQueue {
     use Queueable;
+    
+    /**
+     * Handle the queued event invocation.
+     * 
+     * All event listeners need this method for queued events to work.
+     */
+    public function __invoke()
+    {
+        if (func_num_args() > 0) {
+            $this->handle(func_get_arg(0));
+        } else {
+            Log::error(get_class($this) . '::__invoke() called without arguments');
+        }
+    }
 }
