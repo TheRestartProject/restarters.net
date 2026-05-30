eb7ce47754c047c6ea0d557c09f2e4bc63a748ee
edwh <edward@ehibbert.org.uk>
2026-05-30 23:21:34 +0100

    Migrate events/cantcreate page to Vue (Group 5.2)
    Purely presentational page shown when a restarter (or host with no
    groups) hits /party/create. Moves hard-coded English text into
    lang/{en,fr,fr-BE}/cantcreate.php and renders via a Vue component.
    Adds a feature test asserting the Vue mount renders.

---
+++ lang/en/cantcreate.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'heading' => 'Adding events',
+    'intro' => 'You need to be a host of a group in order to create a new event listing.',
+    'image_alt' => 'Materials for promoting upcoming Restart events',
+    'not_host_q' => 'Not a host?',
+    'not_host_body' => 'You can view already listed events for any groups that you\'re following, and other events near you, on the <a href="/group">Events</a> page.',
+    'missing_event' => '(If an event is missing, please encourage the group host to list it!)',
+    'want_to_add_group_q' => 'Want to add a group that you host?',
+    'want_to_add_group_body' => 'Check whether it\'s already listed on the <a href="/group">Groups</a> page - and if not, please create it!',
+    'start_new_group_q' => 'Want to start a new group?',
+    'start_new_group_body' => 'Find out more here: <a href="https://talk.restarters.net/t/how-to-power-up-community-repair-with-restarters-net/1228/2">How to power up community repair with Restarters.net</a>.',
+];
